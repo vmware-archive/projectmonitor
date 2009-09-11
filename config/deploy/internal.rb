cd73c8fa038eae52c77925b4adbfa9170d78c755
Josh Susser & Serguei Filimonov <ops+jsusser+serguei@pivotallabs.com>
2009-09-11 15:46:14 -0700

    remove clutter

--- config/deploy/internal.rb
+++
@@ -1,6 +0,0 @@
-role :app, "pulse.flood.pivotallabs.com"
-role :web, "pulse.flood.pivotallabs.com"
-role :db, "pulse.flood.pivotallabs.com", :primary => true
-set :use_sudo, false
-set :user, 'pivotal'
-set :non_engineyard, true
--- config/deploy/local.rb
+++
@@ -1,6 +0,0 @@
-role :app, "localhost"
-role :web, "localhost"
-role :db, "localhost", :primary => true
-set :use_sudo, false
-set :non_engineyard, true
-set :no_mongrel_restart, true
