caffa95c6ae8b644835566b344ef1a05d5b84f76
Chad Woolley <thewoolleyman@gmail.com>
2009-09-23 20:09:43 -0700

    clean up geminstaller.yml, add back preinitializer.rb to activate gems via geminstaller

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,1 +1,10 @@
+############# GemInstaller preinitializer - see http://geminstaller.rubyforge.org
+
+require "rubygems"
+require "geminstaller"
+
+# The 'autogem' method will automatically add all gems in the GemInstaller config to your load path, using the 'gem'
+# or 'require_gem' command.  Note that only the *first* version of any given gem will be loaded.
+GemInstaller.autogem("--exceptions")
+
 
