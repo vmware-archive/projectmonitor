90dcf917f730ab1f82d8dfa1ae7b5e35c56a34f7
Edward Hieatt <edwardh@clarion.(none)>
2010-02-01 11:23:52 -0800

    Moving copy of [database.yml.example to database.yml] to cruise_config.rb

--- cruise_config.rb
+++ cruise_config.rb
@@ -2,6 +2,7 @@
 require 'fileutils'
 
 Project.configure do |project|
+  cp File.join(project.path, 'config', 'database.yml.example'), File.join(project.path, 'config', 'database.yml')
   project.email_notifier.emails = ["pivotal-pulse@example.com"]
   ENV['CRUISE_PROJECT_NAME'] = project.name
 end
