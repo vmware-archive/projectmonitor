1135f64845513865d418d5a2b7794b2f7465f2a6
Chad Woolley <thewoolleyman@gmail.com>
2010-03-26 10:20:28 -0700

    ensure that bundled gems are always installed.
    This is a little spammy, but works.  Ticket to reduce spam here:  http://github.com/carlhuda/bundler/issues/issue/224

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -4,12 +4,18 @@ if DEPENDENCY_TOOL == :geminstaller
   require 'rubygems'
   require 'geminstaller'
   require 'geminstaller_rails_preinitializer'
+  # This is to automatically install dependencies on CI boxes
+  GemInstaller.install(['--sudo'])  
 elsif DEPENDENCY_TOOL == :bundler
+  # This is to automatically install dependencies on CI boxes
+  # Requires that you have bundler already installed
+  system('bundle install') || raise("'bundle install' command failed.  gem install bundler if it is not installed")
+
   begin
-    # Require the preresolved locked set of gems.
+    # Sets up the preresolved locked set of gems on the load path.
     require File.expand_path('../../.bundle/environment', __FILE__)
   rescue LoadError
-    # Fallback on doing the resolve at runtime.
+    # Fallback on doing the resolve at runtime and then setting up the load path.
     require "rubygems"
     require "bundler"
     if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.5")
