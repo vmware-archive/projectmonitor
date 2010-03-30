1f2d4f8956632ffdc835d3571c5480a32750eeca
Pivotal Labs <pivotal@spring.nyc.pivotallabs.com>
2010-03-30 11:42:22 -0400

    Adds support for showing TeamCity "running" builds
    Requires Cradiator-TeamCity-Plugin from http://github.com/iwz/Cradiator-TeamCity-Plugin
    This plugin is a fork which adds per-build monitoring instead of only per-project.

--- app/models/team_city_project.rb
+++ app/models/team_city_project.rb
@@ -1,14 +1,8 @@
 class TeamCityProject < Project
-  validates_format_of :feed_url, :with =>  /http:\/\/.*feed.html\?.*buildTypeId.*$/
+  validates_format_of :feed_url, :with =>  /http:\/\/.*\/guestAuth\/cradiator.html\?buildTypeId=.*$/
 
   def build_status_url
-    return nil if feed_url.nil?
-
-    url_components = URI.parse(feed_url)
-    returning("#{url_components.scheme}://#{url_components.host}") do |url|
-      url << ":#{url_components.port}" if url_components.port
-      url << "/cc.xml"
-    end
+    feed_url
   end
 
   def building_parser(content)
--- app/models/team_city_status_parser.rb
+++ app/models/team_city_status_parser.rb
@@ -5,19 +5,19 @@ class TeamCityStatusParser < StatusParser
     def building(content, project)
       building_parser = self.new
       document = Nokogiri::XML.parse(content)
-      p_element = document.xpath("//project[@name=\"#{project.project_name.downcase}\"]")
+      p_element = document.css("Build")
       return building_parser if p_element.empty?
-      building_parser.building = p_element.attribute('activity').value == 'building'
+      building_parser.building = p_element.attribute('activity').value == 'Building'
       building_parser
     end
 
     def status(content)
       status_parser = self.new
       begin
-        latest_build = Nokogiri::XML.parse(content).css('feed entry:first').first
-        status_parser.success = find(latest_build, 'author name').first.content == "Successful Build"
-        status_parser.url = find(latest_build, 'link').first.attribute('href').value
-        pub_date = Time.parse(find(latest_build, 'published').first.content)
+        latest_build = Nokogiri::XML.parse(content).css('Build').first
+        status_parser.success = latest_build.attribute('lastBuildStatus').value == "NORMAL"
+        status_parser.url = latest_build.attribute('webUrl').value
+        pub_date = Time.parse(latest_build.attribute('lastBuildTime').value)
         status_parser.published_at = (pub_date == Time.at(0) ? Clock.now : pub_date).localtime
       rescue
       end
