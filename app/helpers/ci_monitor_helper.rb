64bb66ed6849681c4b4afc3774861da9a1781cf0
Abhi Hiremagalur <pair+abhi@pivotallabs.com>
2010-05-28 15:20:13 -0700

    extracted status messages into a helper in preparation to reuse them in the rss feed

--- app/helpers/ci_monitor_helper.rb
+++ app/helpers/ci_monitor_helper.rb
@@ -4,4 +4,17 @@ module CiMonitorHelper
     count = project.red_build_count
     "(#{count} #{count == 1 ? "build" : "builds"})"
   end
+
+  def status_messages_for(project)
+    messages = []
+    if project.online?
+      messages << ['project_published_at', "Last built #{time_ago_in_words(project.status.published_at)} ago"]
+      if project.red?
+        messages << ['project_red_since', "Red since #{time_ago_in_words(project.red_since)} ago #{build_count_text_for(project)}"]
+      end
+    else
+      messages << ['project_invalid', "Could not retrieve status"]
+    end
+    messages
+  end
 end
