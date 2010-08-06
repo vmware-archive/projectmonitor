d485e4c419bd5398a03e012d4fff39a26cf8e084
Matthew Kocher <ci@cis-MacBook-Pro.local>
2010-08-05 22:56:58 -0700

    trying to make build as automatic as possible

--- ci/ci_build.rb
+++ ci/ci_build.rb
@@ -6,7 +6,8 @@ def run(cmd)
 end
 
 run "bundle install --relock"
-run "rake db:create || true"
+run "RAILS_ENV=test rake db:create || true"
+run "RAILS_ENV=development rake db:create || true"
 run "rake db:migrate"
 run "rake db:schema:load RAILS_ENV=test"
 run "rake default"
