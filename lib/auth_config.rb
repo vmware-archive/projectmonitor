df72024dbe4ae803083bb2a37530692236761c5d
Daniel Stutzman & Michael Barinek <tracker+dstutzman+mbarinek@pivotallabs.com>
2010-10-27 11:49:39 -0600

    auth configuration prefers environment parameters over auth.yml file, also handles missing auth.yml file

--- lib/auth_config.rb
+++ lib/auth_config.rb
@@ -1,10 +1,30 @@
 class AuthConfig
-  config = YAML.load_file("#{Rails.root}/config/auth.yml") || {}
-  auth_config = {}
-  auth_config.update(config[ENV['RAILS_ENV']] || {})
 
-  auth_config.each do |attrib, value|
-    cattr_reader(attrib)
-    class_variable_set("@@#{attrib}", value)
+  # todo - there might be a better pattern here....
+  
+  def self.load_from(path)
+    config = {}
+    config = YAML.load_file(path) if File.exist?(path)
+    @@auth_config = {}
+    @@auth_config.update(config[ENV['RAILS_ENV']] || {})
   end
+
+  self.load_from("#{Rails.root}/config/auth.yml")
+
+  def self.auth_required
+    (ENV['auth_required'] == 'true') || @@auth_config['auth_required']
+  end
+
+  def self.openid_identifier
+    ENV['openid_identifier'] || @@auth_config['openid_identifier']
+  end
+
+  def self.openid_realm
+    ENV['openid_realm'] || @@auth_config['openid_realm']
+  end
+
+  def self.openid_return_to
+    ENV['openid_return_to'] || @@auth_config['openid_return_to']
+  end
+
 end
--- spec/lib/auth_config_spec.rb
+++ spec/lib/auth_config_spec.rb
@@ -1,22 +1,109 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
+# todo - there might be a better pattern here....
+
 describe AuthConfig do
-  it "should have an auth_required" do
-    AuthConfig.auth_required.should == false
+
+  shared_examples_for "prefers environment parameters" do
+    before do
+      ENV['auth_required'] = 'true'
+      ENV['openid_identifier'] = 'google.com'
+      ENV['openid_realm'] = 'http://google.com'
+      ENV['openid_return_to'] = 'http://google.com/openid/success'
+    end
+
+    after do
+      ENV['auth_required'] = nil
+      ENV['openid_identifier'] = nil
+      ENV['openid_realm'] = nil
+      ENV['openid_return_to'] = nil
+    end
+
+    it "should have an auth_required" do
+      AuthConfig.auth_required.should be_true
+    end
+
+    describe "with openid" do
+      it "should have an openid identifier" do
+        AuthConfig.openid_identifier.should == "google.com"
+      end
+
+      it "should have an openid realm" do
+        AuthConfig.openid_realm.should == "http://google.com"
+      end
+
+      it "should have an openid return_to" do
+        AuthConfig.openid_return_to.should == "http://google.com/openid/success"
+      end
+
+    end
   end
 
-  describe "openid config" do
-    it "should have an openid identifier" do
-      AuthConfig.openid_identifier.should == "example.com"
+  describe "should prefer environment parameter over yml" do
+    it_should_behave_like "prefers environment parameters"
+  end
+
+  describe "should use yml configuration" do
+
+    it "should have an auth_required" do
+      AuthConfig.auth_required.should == false
     end
 
-    it "should have an openid realm" do
-      AuthConfig.openid_realm.should == "http://example.com"
+    describe "with openid" do
+      it "should have an openid identifier" do
+        AuthConfig.openid_identifier.should == "example.com"
+      end
+
+      it "should have an openid realm" do
+        AuthConfig.openid_realm.should == "http://example.com"
+      end
+
+      it "should have an openid return_to" do
+        AuthConfig.openid_return_to.should == "http://example.com/openid/success"
+      end
     end
 
-    it "should have an openid return_to" do
-      AuthConfig.openid_return_to.should == "http://example.com/openid/success"
+  end
+
+  describe "should prefer environment parameters over a non-existent file" do
+    before do
+      AuthConfig.load_from("bad_path")
     end
+
+    after do
+      AuthConfig.load_from("#{Rails.root}/config/auth.yml")
+    end
+
+    it_should_behave_like "prefers environment parameters"
+  end
+
+  describe "should be able to handle a non-existent file" do
+
+    before do
+      AuthConfig.load_from("bad_path")
+    end
+
+    after do
+      AuthConfig.load_from("#{Rails.root}/config/auth.yml")
+    end
+
+    it "should have an auth_required" do
+      AuthConfig.auth_required.should be_false
+    end
+
+    describe "with openid" do
+      it "should have an openid identifier" do
+        AuthConfig.openid_identifier.should be_nil
+      end
+
+      it "should have an openid realm" do
+        AuthConfig.openid_realm.should be_nil
+      end
+
+      it "should have an openid return_to" do
+        AuthConfig.openid_return_to.should be_nil
+      end
+    end
+
   end
 end
-
