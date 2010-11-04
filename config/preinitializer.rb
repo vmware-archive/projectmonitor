c6dd2ef68e8ee16cb1f500eb76a2352290bd9b9d
Josh Susser & Tyler Schultz <pair+jsusser+tyler@pivotallabs.com>
2010-11-04 16:06:17 -0700

    inline CI build command, simplify bundler usage

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,32 +1,20 @@
-if ENV['IS_CI_BOX']
-  unless system('bundle check')
-    puts "IS_CI_BOX is set, running `bundle install`..."
-    system('bundle install') || raise("'bundle install' command failed. Install bundler with `gem install bundler`.")
-  end
+begin
+  require "rubygems"
+  require "bundler"
+rescue LoadError
+  raise "Could not load the bundler gem. Install it with `gem install bundler`."
+end
+
+if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.24")
+  raise RuntimeError, "Your bundler version is too old for Rails 2.3." +
+   "Run `gem install bundler` to upgrade."
 end
 
 begin
-  # Sets up the preresolved locked set of gems on the load path.
-  require File.expand_path('../../.bundle/environment', __FILE__)
-rescue LoadError
-  # Fallback on doing the resolve at runtime and then setting up the load path.
-  begin
-    require "rubygems"
-    require "bundler"
-  rescue LoadError
-    raise "Could not load the bundler gem. Install it with `gem install bundler`."
-  end
-  
-  if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.24")
-    raise RuntimeError, "Your bundler version is too old.  Run `gem install bundler` to upgrade."
-  end
-  
-  begin
-    # Set up load paths for all bundled gems
-    ENV["BUNDLE_GEMFILE"] = File.expand_path("Gemfile", RAILS_ROOT)
-    Bundler.setup
-  rescue Bundler::GemNotFound
-    raise RuntimeError, "Bundler couldn't find some gems.  Did you run `bundle install`?  " +
-      "If this is a CI box, the IS_CI_BOX env var should be set to automatically run `bundle install` (currently it #{ENV['IS_CI_BOX'] ? 'IS' : 'IS NOT'} set)"
-  end
+  # Set up load paths for all bundled gems
+  ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)
+  Bundler.setup
+rescue Bundler::GemNotFound
+  raise RuntimeError, "Bundler couldn't find some gems." +
+    "Did you run `bundle install`?"
 end
--- cruise_config.rb
+++ cruise_config.rb
@@ -3,8 +3,7 @@ require 'fileutils'
 
 Project.configure do |project|
   FileUtils.cp File.join(project.path, 'work', 'config', 'database.yml.example'), File.join(project.path, 'work', 'config', 'database.yml')
-  FileUtils.cp File.join(project.path, 'work', 'config', 'initializers', 'site_keys.rb.example'), File.join(project.path, 'work', 'config', 'initializers', 'site_keys.rb')
   project.email_notifier.emails = ["pivotal-cimonitor@example.com"]
   ENV['CRUISE_PROJECT_NAME'] = project.name
-  project.build_command = 'ruby ci/ci_build.rb'
+  project.build_command = "bundle install && rake db:migrate && rake db:schema:load RAILS_ENV=test && rake default"
 end
