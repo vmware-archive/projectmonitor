9f846a17af2f15ad446439fc99a9e79fce110442
David Goudreau & Glenn Jahnke <pair+david+glennj@pivotallabs.com>
2011-01-06 11:56:23 -0800

    when searching for a tags, only show the aggregate project and not the children if they both have the tag

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,9 @@
 class DashboardsController < ApplicationController
   def show
     if params[:tags]
-      @projects = Project.find_tagged_with(params[:tags], :conditions => {:enabled => true}, :order => 'name', :include => :statuses)
-      @projects += AggregateProject.find_tagged_with(params[:tags], :conditions => {:enabled => true}, :order => 'name')
+      aggregate_projects = AggregateProject.find_tagged_with(params[:tags], :conditions => {:enabled => true}, :order => 'name')
+      projects = Project.find_tagged_with(params[:tags], :conditions => {:enabled => true}, :order => 'name', :include => :statuses)
+      @projects = projects.reject {|project| aggregate_projects.include?(project.aggregate_project)} + aggregate_projects
     else
       @projects = Project.standalone
       @projects += AggregateProject.with_projects.flatten
