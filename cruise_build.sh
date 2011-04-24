067b31004983a0112737d756b650fdad2d7c209e
Matthew Kocher <kocher@gmail.com>
2011-04-24 12:21:33 -0700

    using the mysql2 gem

--- cruise_build.sh
+++ cruise_build.sh
@@ -11,4 +11,9 @@ echo USER=$USER && ruby --version && which ruby && which bundle
 # conditionally install project gems from Gemfile
 bundle check || bundle install --without postgres || exit 1
 
+rake setup
+
+RAILS_ENV=test rake db:create || true
+RAILS_ENV=development rake db:create || true
+
 rake cruise
