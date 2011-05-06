fe2308318a9165ee3bfd8445cdcdc49789fa8fa7
Luke Crawford and Dave Goddard <pair+luke+dgoddard@pivotallabs.com>
2011-05-06 11:18:51 -0400

    "Enable" messages in UI, add styles, add expiry to messages.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,29 @@
 class Message < ActiveRecord::Base
+  DURATIONS_SELECT = [
+    ["30 minutes", 30.minutes],
+    ["1 hour",     1.hour],
+    ["2 hours",    2.hours],
+    ["8 hours",    8.hours],
+    ["1 day",      1.day],
+    ["2 days",     2.days]
+  ]
+
   validates_presence_of :text
+
+  acts_as_taggable
+
+  def expires_in
+    return if expires_at.blank?
+    return if created_at.blank?
+
+    expires_at - created_at
+  end
+
+  def expires_in=(seconds)
+    self.expires_at = if seconds.to_i > 0 && created_at.present?
+      created_at + seconds.to_i
+    else
+      Time.now
+    end
+  end
 end
