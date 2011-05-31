56b428753b11c85c569352cfac81cdb9c31d0a68
Joseph Palermo & Josh Susser <pair+joseph+jsusser@pivotallabs.com>
2011-05-31 15:35:00 -0700

    make postgresql happy

--- db/migrate/20110325235043_add_indexes_to_project_statuses.rb
+++ db/migrate/20110325235043_add_indexes_to_project_statuses.rb
@@ -1,9 +1,9 @@
 class AddIndexesToProjectStatuses < ActiveRecord::Migration
   def self.up
-    add_index :project_statuses, [:project_id, :online, :published_at]
+    add_index :project_statuses, [:project_id, :online, :published_at], :name => 'index_project_statuses_on_project_id_and_others'
   end
 
   def self.down
-    remove_index :project_statuses, [:project_id, :online, :published_at]
+    remove_index :project_statuses, :name => 'index_project_statuses_on_project_id_and_others'
   end
 end
