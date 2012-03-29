706e2405c7b9ed203a7a93473e534179e6b8ecb4
Dirk Kelly & Matt Parker <pair+dirk+matt@pivotallabs.com>
2012-03-29 10:07:01 -0400

    Turn StatusFetcher into a well tested Module

--- lib/status_fetcher.rb
+++ lib/status_fetcher.rb
@@ -1,49 +1,40 @@
-class StatusFetcher
-  def initialize(url_retriever = UrlRetriever)
+module StatusFetcher
+  extend self
+
+  class Job < Struct.new(:project)
+    def perform
+      StatusFetcher.retrieve_status_for(project)
+      StatusFetcher.retrieve_building_status_for(project)
+      project.set_next_poll!
+    end
   end
 
   def fetch_all
     projects = Project.find(:all)
-    projects.reject! {|project| !project.needs_poll?}
+    projects.select! &:needs_poll?
     projects.each do |project|
-      retrieve_status_for(project)
-      retrieve_building_status_for(project)
-      project.set_next_poll!
+      Delayed::Job.enqueue StatusFetcher::Job.new(project)
     end
-    0
   end
 
   def retrieve_status_for(project)
-    status = ProjectStatus.new(:online => false, :success => false)
-    status.error = http_errors_for(project) do
-      content = UrlRetriever.retrieve_content_at(project.feed_url, project.auth_username, project.auth_password)
-      status = project.parse_project_status(content)
-      status.online = true
-    end
-    project.statuses.build(status.attributes).save unless project.status.match?(status)
+    content = UrlRetriever.retrieve_content_at(project.feed_url, project.auth_username, project.auth_password)
+    status = project.parse_project_status(content)
+    status.online = true
+    project.statuses.create(status.attributes) unless project.status.match?(status)
+
+  rescue Net::HTTPError => e
+    error = "HTTP Error retrieving status for project '##{project.id}': #{e.message}"
+    project.statuses.create(:error => error) unless project.status.error == error
   end
-  # handle_asynchronously :retrieve_status_for, :queue => 'project_status'
 
   def retrieve_building_status_for(project)
-    status = BuildingStatus.new(false)
-    status.error = http_errors_for(project) do
-      content = UrlRetriever.retrieve_content_at(project.build_status_url, project.auth_username, project.auth_password)
-      status = project.parse_building_status(content)
-    end
+    content = UrlRetriever.retrieve_content_at(project.build_status_url, project.auth_username, project.auth_password)
+    status = project.parse_building_status(content)
     project.update_attribute(:building, status.building?)
-  end
-  handle_asynchronously :retrieve_building_status_for, :queue => 'build_status'
-
-  private
-
-  def http_errors_for(project)
-    yield
-    nil
-  rescue URI::InvalidURIError => e
-    "Invalid URI for project '#{project}': #{e.message}"
   rescue Net::HTTPError => e
-    "HTTP Error retrieving status for project '#{project}': #{e.message}"
-  rescue Exception => e
-    "Retrieve Status failed for project '#{project}'.  Exception: '#{e.class}: #{e.message}'\n#{e.backtrace.join("\n")}"
+    project.update_attribute(:building, false)
   end
+
 end
+
