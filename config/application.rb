1f5766f6ec0a1fec9eb8f8e52c8986d00393e098
Dirk Kelly & Matt Parker <pair+dirk+matt@pivotallabs.com>
2012-03-30 10:53:49 -0400

    initialization fix for heroku cedar

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,7 @@ module CiMonitor
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
