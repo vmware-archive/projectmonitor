d294d0c0f77a105ebb4fccf61e945da199114c3b
Matt Parker + Dimitri Roche <pairs+mp+dr@pivotallabs.com>
2012-04-17 16:54:24 -0400

    Added rendering of empty project tiles

---
+++ lib/grid_collection.rb
@@ -0,0 +1,17 @@
+require 'delegate'
+
+class GridCollection < SimpleDelegator
+  LIMITS = [15, 24, 48]
+
+  def initialize(array)
+    @upper_limit = LIMITS.find { |limit| array.count <= limit }
+    validate_size
+    @entries = array + Array.new(@upper_limit - array.size)
+    super @entries
+  end
+
+  private
+  def validate_size
+    raise ArgumentError, "We never anticipated more than 48 projects. Sorry." unless @upper_limit
+  end
+end
