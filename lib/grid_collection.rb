369d3f097f1abb7f209220038006bcbb66eea02a
Dimitri Roche + Trace Wax <pairs+dr+tw@pivotallabs.com>
2012-04-19 11:46:11 -0400

    Toggle number of tiles

--- lib/grid_collection.rb
+++ lib/grid_collection.rb
@@ -3,8 +3,8 @@ require 'delegate'
 class GridCollection < SimpleDelegator
   LIMITS = [15, 24, 48]
 
-  def initialize(array)
-    @upper_limit = LIMITS.find { |limit| array.count <= limit }
+  def initialize(array, tile_count = nil)
+    @upper_limit = tile_count || LIMITS.find { |limit| array.count <= limit }
     validate_size
     @entries = array + Array.new(@upper_limit - array.size)
     super @entries
