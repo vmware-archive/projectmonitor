d6d8913ee2fb4557b1eda09e97c1854b590b8675
Dimitri Roche + Trace Wax <pairs+dr+tw@pivotallabs.com>
2012-04-19 11:46:11 -0400

    Toggle number of tiles

--- spec/lib/grid_collection_spec.rb
+++ spec/lib/grid_collection_spec.rb
@@ -1,88 +1,108 @@
 require_relative '../../lib/grid_collection'
 
 describe ::GridCollection do
-  subject { GridCollection.new collection }
+  context "without tile count" do
+    subject { GridCollection.new collection }
 
-  context "instantiated with an empty collection" do
-    let(:collection) { [] }
+    context "instantiated with an empty collection" do
+      let(:collection) { [] }
 
-    its(:count) { should == 15}
+      its(:count) { should == 15}
 
-    it "should make all the elements nil" do
-      subject.all?(&:nil?).should be_true
+      it "should make all the elements nil" do
+        subject.all?(&:nil?).should be_true
+      end
+    end
+
+    context "instantiated with a collection of size < 15" do
+      let(:collection) { [1,2,3] }
+
+      its(:count) { should == 15}
+
+      it "should pad the collection with nil objects" do
+        subject.select(&:nil?).count.should == 12
+        subject[0...3].should == collection
+      end
+    end
+
+    context "instantiated with a collection of size = 15" do
+      let(:collection) { (0...15).to_a }
+
+      its(:count) { should == 15}
+
+      it "should pad the collection with nil objects" do
+        subject.select(&:nil?).count.should == 0
+        subject.should == collection
+      end
+    end
+
+    context "instantiated with a collection of size > 15 AND < 24" do
+      let(:collection) { (0...16).to_a }
+
+      its(:count) { should == 24}
+
+      it "should pad the collection with nil objects" do
+        subject.select(&:nil?).count.should == 8
+        subject[0...16].should == collection
+      end
+    end
+
+    context "instantiated with a collection of size = 24" do
+      let(:collection) { (0...24).to_a }
+
+      its(:count) { should == 24}
+
+      it "should pad the collection with nil objects" do
+        subject.select(&:nil?).count.should == 0
+        subject.should == collection
+      end
+    end
+
+
+    context "instantiated with a collection of size > 24 AND < 48" do
+      let(:collection) { (0...25).to_a }
+
+      its(:count) { should == 48}
+
+      it "should pad the collection with nil objects" do
+        subject.select(&:nil?).count.should == (48 - 25)
+        subject[0...25].should == collection
+      end
+    end
+
+    context "instantiated with a collection of size = 48" do
+      let(:collection) { (0...48).to_a }
+
+      its(:count) { should == 48}
+
+      it "should pad the collection with nil objects" do
+        subject.select(&:nil?).count.should == 0
+        subject.should == collection
+      end
+    end
+
+    context "instantiated with a collection size > 48" do
+      let(:collection) { (0...50).to_a }
+
+      specify { expect { subject }.to raise_exception(ArgumentError, "We never anticipated more than 48 projects. Sorry.") }
     end
   end
 
-  context "instantiated with a collection of size < 15" do
-    let(:collection) { [1,2,3] }
-
-    its(:count) { should == 15}
-
-    it "should pad the collection with nil objects" do
-      subject.select(&:nil?).count.should == 12
-      subject[0...3].should == collection
+  context "with tile_count" do
+    subject { GridCollection.new [], tile_count }
+    context "when present" do
+      let(:tile_count) { 24 }
+      its(:size) { should == tile_count }
     end
-  end
 
-  context "instantiated with a collection of size = 15" do
-    let(:collection) { (0...15).to_a }
-
-    its(:count) { should == 15}
-
-    it "should pad the collection with nil objects" do
-      subject.select(&:nil?).count.should == 0
-      subject.should == collection
+    context "when not present" do
+      subject { GridCollection.new [] }
+      its(:size) { should == GridCollection::LIMITS.first }
     end
-  end
 
-  context "instantiated with a collection of size > 15 AND < 24" do
-    let(:collection) { (0...16).to_a }
-
-    its(:count) { should == 24}
-
-    it "should pad the collection with nil objects" do
-      subject.select(&:nil?).count.should == 8
-      subject[0...16].should == collection
+    context "when nil" do
+      let(:tile_count) { nil }
+      its(:size) { should == GridCollection::LIMITS.first }
     end
-  end
-
-  context "instantiated with a collection of size = 24" do
-    let(:collection) { (0...24).to_a }
-
-    its(:count) { should == 24}
-
-    it "should pad the collection with nil objects" do
-      subject.select(&:nil?).count.should == 0
-      subject.should == collection
-    end
-  end
-
-
-  context "instantiated with a collection of size > 24 AND < 48" do
-    let(:collection) { (0...25).to_a }
-
-    its(:count) { should == 48}
-
-    it "should pad the collection with nil objects" do
-      subject.select(&:nil?).count.should == (48 - 25)
-      subject[0...25].should == collection
-    end
-  end
-
-  context "instantiated with a collection of size = 48" do
-    let(:collection) { (0...48).to_a }
-
-    its(:count) { should == 48}
-
-    it "should pad the collection with nil objects" do
-      subject.select(&:nil?).count.should == 0
-      subject.should == collection
-    end
-  end
-
-  context "instantiated with a collection size > 48" do
-    let(:collection) { (0...50).to_a }
-
-    specify { expect { subject }.to raise_exception(ArgumentError, "We never anticipated more than 48 projects. Sorry.") }
   end
 end
---
+++ spec/requests/dashboard_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+feature "Dashboard" do
+  let(:password) { "monkey" }
+  let(:user) { FactoryGirl.create(:user, password: password) }
+  before { visit root_path }
+  scenario "user toggles tile layout" do
+    page.should have_css("ol.projects.tiles_15")
+
+    click_link "24"
+    page.should have_css("ol.projects.tiles_24")
+
+    click_link "15"
+    page.should have_css("ol.projects.tiles_15")
+  end
+end
