40fdd4ad44bf6aebbc027837e2d3fff3cf3057b3
JT Archie & Trace Wax <pair+jt+trace@pivotallabs.com>
2012-04-20 17:04:40 -0400

    refresh projects specifically to a projects type on the dashboard

--- spec/javascripts/refreshSpec.js
+++ spec/javascripts/refreshSpec.js
@@ -5,25 +5,40 @@ describe('refresh', function() {
         "<li class='project success' id='project_1' data-id='1'></li>",
         "<li class='project failure' id='project_2' data-id='2'></li>",
         "<li class='project failure' id='project_3' data-id='3'></li>",
-        "<li class='project aggregate success' id='project_4' data-id='4'></li>",
+        "<li class='project aggregate success' id='aggregate_project_4' data-id='4'></li>",
       "</ul>"
     ].join("\n");
     setFixtures(fixtures);
   });
 
-  afterEach(function(){
-    $.ajax.reset();
-  });
-
   it("should call $.get for projects and aggregates", function() {
-    var spyOnGet = spyOn(jQuery, "ajax");
-
     refresh();
 
-    expect(spyOnGet).toHaveBeenCalled();
-    expect(spyOnGet.argsForCall[0][0].url).toBe("/projects/1/status");
-    expect(spyOnGet.argsForCall[1][0].url).toBe("/projects/2/status");
-    expect(spyOnGet.argsForCall[2][0].url).toBe("/projects/3/status");
-    expect(spyOnGet.argsForCall[3][0].url).toBe("/aggregate_projects/4/status");
+    expect(ajaxRequests.length).toEqual(4);
+    expect(ajaxRequests[0].url).toBe("/projects/1/status");
+    expect(ajaxRequests[1].url).toBe("/projects/2/status");
+    expect(ajaxRequests[2].url).toBe("/projects/3/status");
+    expect(ajaxRequests[3].url).toBe("/aggregate_projects/4/status");
+  });
+
+  describe("when a requjest comes back", function() {
+    beforeEach(function() {
+      refresh();
+      mostRecentAjaxRequest().response({
+        status: 200,
+        responseText: "<li class='grid_4' id='aggregate_project_4' data-id='4'>Hello World</li>"
+      });
+    });
+
+    it("replaces the correct DOM element", function() {
+      expect($("#aggregate_project_4").text()).toContain("Hello World");
+    });
+
+    it("uses the previous items classes", function() {
+      expect($("#aggregate_project_4")).toHaveClass("project");
+      expect($("#aggregate_project_4")).toHaveClass("aggregate");
+      expect($("#aggregate_project_4")).toHaveClass("success");
+      expect($("#aggregate_project_4")).not.toHaveClass("grid_4");
+    });
   });
 });
