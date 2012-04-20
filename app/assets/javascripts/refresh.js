86a9f1ba560489531e4f3ac941f9753cb2a0a4ed
JT Archie & Trace Wax <pair+jt+trace@pivotallabs.com>
2012-04-20 16:16:12 -0400

    Preserve grid layout when projects refresh

--- app/assets/javascripts/refresh.js
+++ app/assets/javascripts/refresh.js
@@ -5,6 +5,7 @@ var refresh, scheduleRefresh;
 
 refresh = function() {
   $(".project").each(function(index,element) {
+    var current_classes = $(element).attr("class");
     var project_id = $(element).attr('data-id');
     var project_type = $(element).hasClass('aggregate') ? 'aggregate_project' : 'project';
     $.ajax({
@@ -12,6 +13,7 @@ refresh = function() {
       method: 'GET',
       complete: function(data) {
         $('#project_'+project_id).replaceWith(data.responseText);
+        $('#project_'+project_id).attr("class", current_classes);
       }
     });
   });
