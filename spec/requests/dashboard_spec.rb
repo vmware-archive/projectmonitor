c546c7110ffc855860033e482b72a4f39c4dd008
Matt Parker + Trace Wax <pairs+mp+tw@pivotallabs.com>
2012-04-25 14:47:52 -0400

    Improve organization of dashboard spec

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 feature "Dashboard" do
   let(:password) { "monkey" }
   let(:user) { FactoryGirl.create(:user, password: password) }
+
   context "looking at projects" do
     before { visit root_path }
+
     scenario "user toggles tile layout" do
       page.should have_css("ol.projects.tiles_15")
 
@@ -20,34 +22,36 @@ feature "Dashboard" do
       click_link "15"
       page.should have_css("ol.projects.tiles_15")
     end
+
+    scenario "user toggles the location view" do
+      15.times { FactoryGirl.create(:project, location: "Jamaica") }
+      5.times { FactoryGirl.create(:project, location: "New York") }
+      5.times { FactoryGirl.create(:project, location: "San Francisco") }
+      2.times { FactoryGirl.create(:project) }
+
+      visit root_path
+
+      page.should have_no_content("Jamaica")
+      page.should have_no_content("New York")
+      page.should have_no_content("San Francisco")
+
+      click_link "locations"
+
+      page.should have_content("Jamaica")
+      page.should have_content("New York")
+      page.should have_content("San Francisco")
+
+      click_link "48"
+
+      page.should have_no_content("Jamaica")
+      page.should have_no_content("New York")
+      page.should have_no_content("San Francisco")
+    end
   end
 
-  scenario "user toggles the location view" do
-    15.times { FactoryGirl.create(:project, location: "Jamaica") }
-    5.times { FactoryGirl.create(:project, location: "New York") }
-    5.times { FactoryGirl.create(:project, location: "San Francisco") }
-    2.times { FactoryGirl.create(:project) }
-
-    visit root_path
-
-    page.should have_no_content("Jamaica")
-    page.should have_no_content("New York")
-    page.should have_no_content("San Francisco")
-
-    click_link "locations"
-
-    page.should have_content("Jamaica")
-    page.should have_content("New York")
-    page.should have_content("San Francisco")
-
-    click_link "48"
-
-    page.should have_no_content("Jamaica")
-    page.should have_no_content("New York")
-    page.should have_no_content("San Francisco")
-  end
 
   context "the build RSS feed" do
+    let!(:project) { FactoryGirl.create(:project, code: "MyCode", statuses: [ProjectStatus.new]) }
     before { visit '/builds.rss' }
 
     scenario "user sees an RSS feed of current builds statuses" do
