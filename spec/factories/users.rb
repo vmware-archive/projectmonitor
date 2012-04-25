69b1cc3bf373aab4388ebac4becac20696b048fd
Matt Parker + Trace Wax <pairs+mp+tw@pivotallabs.com>
2012-04-25 14:48:19 -0400

    Prevent periodic spec failures due to punctuation in names generated by Faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    login { Faker::Name.first_name }
+    login { Faker::Name.first_name.downcase.gsub(/[^a-z0-9\.-_@]/, '') }
     email { Faker::Internet.email }
     password { "monkey" }
     password_confirmation { password }
