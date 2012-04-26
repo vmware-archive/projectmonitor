9a5a4236c0866f571a1e428e54d429e20cfa9b1d
Peter Jaros <pair+pjaros@pivotallabs.com>
2012-04-26 15:22:12 -0400

    Implement TrackerApi using pivotal-tracker gem.

--- lib/tracker_api.rb
+++ lib/tracker_api.rb
@@ -1,22 +1,15 @@
-require 'open-uri'
-require 'active_support/all'
+require 'pivotal_tracker'
 
 class TrackerApi
   def initialize(token)
     @token = token
+    PivotalTracker::Client.token = token
   end
 
-  def fetch_current_iteration(project_id)
-    url = "http://www.pivotaltracker.com/services/v3/projects/#{project_id}/iterations/current"
-
-    Hash.from_xml(fetch_xml_response(url))["iterations"].first
-  end
-
-
-  private
-
-  def fetch_xml_response(url)
-    Kernel.open(url, { "X-TrackerToken" => @token})
-          .read
+  def delivered_stories_count(project_id)
+    PivotalTracker::Project.find(project_id)
+                             .stories
+                             .all(current_state: "delivered")
+                               .count
   end
 end
