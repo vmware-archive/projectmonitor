4bf108305163c0e590534415a3bba479b7523b1c
Peter Jaros <pair+pjaros@pivotallabs.com>
2012-04-27 17:59:24 -0400

    Stop silently rescuing errors.
    All this did was give us mysterious ProjectStatuses with nil values.
    Better to see the errors in the DJ log.

--- app/models/team_city_project.rb
+++ app/models/team_city_project.rb
@@ -9,23 +9,24 @@ class TeamCityProject < Project
 
   def parse_building_status(content)
     status = super(content)
+
     document = Nokogiri::XML.parse(content)
     p_element = document.css("Build")
     return status if p_element.empty?
     status.building = p_element.attribute('activity').value == 'Building'
+
     status
   end
 
   def parse_project_status(content)
     status = super(content)
-    begin
-      latest_build = Nokogiri::XML.parse(content).css('Build').first
-      status.success = latest_build.attribute('lastBuildStatus').value == "NORMAL"
-      status.url = latest_build.attribute('webUrl').value
-      pub_date = Time.parse(latest_build.attribute('lastBuildTime').value)
-      status.published_at = (pub_date == Time.at(0) ? Clock.now : pub_date).localtime
-    rescue
-    end
+
+    latest_build = Nokogiri::XML.parse(content).css('Build').first
+    status.success = latest_build.attribute('lastBuildStatus').value == "NORMAL"
+    status.url = latest_build.attribute('webUrl').value
+    pub_date = Time.parse(latest_build.attribute('lastBuildTime').value)
+    status.published_at = (pub_date == Time.at(0) ? Clock.now : pub_date).localtime
+
     status
   end
 end
--- app/models/team_city_rest_project.rb
+++ app/models/team_city_rest_project.rb
@@ -10,32 +10,33 @@ class TeamCityRestProject < Project
 
   def parse_building_status(content)
     status = super(content)
+
     document = Nokogiri::XML.parse(content)
     p_element = document.css("build").first
     return status if p_element.nil? || p_element.attribute('running').nil?
     status.building = p_element.attribute('running').value == 'true'
+
     status
   end
 
   def parse_project_status(content)
     status = super(content)
-    begin
-      latest_build = Nokogiri::XML.parse(content).css('build').first
-      status.success = latest_build.attribute('status').value == "SUCCESS"
-      status.url = latest_build.attribute('webUrl').value
 
-      status.published_at = if latest_build.attribute('startDate').present?
-                              Time.parse(latest_build.attribute('startDate').value).localtime
+    latest_build = Nokogiri::XML.parse(content).css('build').first
+    status.success = latest_build.attribute('status').value == "SUCCESS"
+    status.url = latest_build.attribute('webUrl').value
+
+    status.published_at = if latest_build.attribute('startDate').present?
+                            Time.parse(latest_build.attribute('startDate').value).localtime
+                          else
+                            previous_status = statuses.first
+                            if previous_status && status.url == previous_status.url && status.success == previous_status.success
+                              previous_status.published_at # no change
                             else
-                              previous_status = statuses.first
-                              if previous_status && status.url == previous_status.url && status.success == previous_status.success
-                                previous_status.published_at # no change
-                              else
-                                Clock.now.localtime
-                              end
+                              Clock.now.localtime
                             end
-    rescue
-    end
+                          end
+
     status
   end
 
