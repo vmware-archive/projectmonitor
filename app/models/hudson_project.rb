36b9ce43e335e1ac1812040018441ecf9cce2c32
Peter Jaros <pair+pjaros@pivotallabs.com>
2012-04-27 17:59:24 -0400

    Stop silently rescuing errors.
    All this did was give us mysterious ProjectStatuses with nil values.
    Better to see the errors in the DJ log.

--- app/models/hudson_project.rb
+++ app/models/hudson_project.rb
@@ -18,23 +18,24 @@ class HudsonProject < Project
 
   def parse_building_status(content)
     status = super(content)
+
     document = Nokogiri::XML.parse(content.downcase)
     p_element = document.xpath("//project[@name=\"#{project_name.downcase}\"]")
     return status if p_element.empty?
     status.building = p_element.attribute('activity').value == 'building'
+
     status
   end
 
   def parse_project_status(content)
     status = super(content)
-    begin
-      latest_build = Nokogiri::XML.parse(content.downcase).css('feed entry:first').first
-      status.success = !!(find(latest_build, 'title').first.content =~ /success|stable|back to normal/)
-      status.url = find(latest_build, 'link').first.attribute('href').value
-      pub_date = Time.parse(find(latest_build, 'published').first.content)
-      status.published_at = (pub_date == Time.at(0) ? Clock.now : pub_date).localtime
-    rescue
-    end
+
+    latest_build = Nokogiri::XML.parse(content.downcase).css('feed entry:first').first
+    status.success = !!(find(latest_build, 'title').first.content =~ /success|stable|back to normal/)
+    status.url = find(latest_build, 'link').first.attribute('href').value
+    pub_date = Time.parse(find(latest_build, 'published').first.content)
+    status.published_at = (pub_date == Time.at(0) ? Clock.now : pub_date).localtime
+
     status
   end
 
