cda92f85ec9165de74f0dc800283f0f87653e85a
Matthew Horan <pair+mhoran@pivotallabs.com>
2012-05-09 11:36:02 -0400

    Refresh browser instances on deploy

---
+++ app/assets/javascripts/versionCheck.js
@@ -0,0 +1,43 @@
+var VersionCheck = function() {
+  var versionCheckTimeout = 60 * 1000; // 1 minute
+
+  var currentTimeout;
+
+  var versionRequest = function(success) {
+    $.ajax({
+      url: '/revision',
+      method: 'GET',
+      success: success,
+    });
+  };
+
+  var scheduleRefresh = function() {
+    clearTimeout(currentTimeout);
+    currentTimeout = setTimeout(VersionCheck.checkVersion, versionCheckTimeout);
+  };
+
+  return {
+    init: function() {
+      versionRequest(function(response) {
+        window.currentVersion = response;
+      });
+      scheduleRefresh();
+    },
+
+    checkVersion: function() {
+      versionRequest(function(response) {
+        if (response != window.currentVersion)
+          window.location.reload();
+      });
+      scheduleRefresh();
+    },
+
+    currentVersion: function() {
+      return window.currentVersion;
+    }
+  }
+}();
+
+$(function() {
+  VersionCheck.init();
+});
