250d3f9cd69b73a14435339810743b5a7e5d7a4e
Matthew Horan <pair+mhoran@pivotallabs.com>
2012-05-09 14:50:42 -0400

    Write out the revision to Rails.root/REVISION and commit to deploy branch for Heroku

--- spec/controllers/revisions_controller_spec.rb
+++ spec/controllers/revisions_controller_spec.rb
@@ -5,7 +5,7 @@ describe RevisionsController do
     let(:revision) { '123' }
 
     before do
-      File.stub(:read).with(File.join(Rails.root, 'REVISION')).and_return(revision)
+      File.stub(:read).with(RevisionsController::REVISION_PATH).and_return(revision)
     end
 
     it 'returns the current revision' do
