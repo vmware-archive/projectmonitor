cfbf33fdf19402dba617a629a64273b4d463a137
Matthew Horan <pair+mhoran@pivotallabs.com>
2012-05-09 12:02:32 -0400

    Use a default revision if the REVISION file does not exist

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -1,5 +1,15 @@
 class RevisionsController < ApplicationController
+  DEFAULT_REVISION = '1'
+
   def show
-    render :text => File.read(File.join(Rails.root, 'REVISION'))
+    render :text => revision
+  end
+
+  private
+
+  def revision
+    File.read(File.join(Rails.root, 'REVISION'))
+  rescue Errno::ENOENT
+    DEFAULT_REVISION
   end
 end
