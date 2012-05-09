d551b6b20a6c2232f7ceef90bb56e731f2cbc6d9
Matthew Horan <pair+mhoran@pivotallabs.com>
2012-05-09 12:02:32 -0400

    Use a default revision if the REVISION file does not exist

--- spec/controllers/revisions_controller_spec.rb
+++ spec/controllers/revisions_controller_spec.rb
@@ -1,18 +1,31 @@
 require 'spec_helper'
 
 describe RevisionsController do
-  let(:revision) { '123' }
+  context 'with a REVISION file in the Rails root' do
+    let(:revision) { '123' }
 
-  before do
-    File.stub(:read).with(File.join(Rails.root, 'REVISION')).and_return(revision)
+    before do
+      File.stub(:read).with(File.join(Rails.root, 'REVISION')).and_return(revision)
+    end
+
+    it 'returns the current revision' do
+      get :show
+      response.body.should == revision
+    end
+
+    it 'should route GET /revision to RevisionsController#show' do
+      { get: '/revision' }.should route_to(:controller => 'revisions', :action => 'show')
+    end
   end
 
-  it 'returns the current revision' do
-    get :show
-    response.body.should == revision
-  end
+  context 'with no REVISION file in the Rails root' do
+    it 'returns the current revision' do
+      get :show
+      response.body.should == RevisionsController::DEFAULT_REVISION
+    end
 
-  it 'should route GET /revision to RevisionsController#show' do
-    { get: '/revision' }.should route_to(:controller => 'revisions', :action => 'show')
+    it 'should route GET /revision to RevisionsController#show' do
+      { get: '/revision' }.should route_to(:controller => 'revisions', :action => 'show')
+    end
   end
 end
