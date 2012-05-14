8900e2c308399f3527d04e1cd3a7dcddf4fac78d
Cameron Cundiff & Matthew Horan <pair+ccundiff+mhoran@pivotallabs.com>
2012-05-14 18:09:37 -0400

    We've got a spinner!

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,8 +3,27 @@
 //= require twitter
 //= require versionCheck
 //= require projectCheck
+//= require spin
 
 $(function() {
   VersionCheck.init();
   ProjectCheck.init();
+
+  $('.building-indicator').spin({radius:8, length:6, width:3, lines:12, top:6, left:8});
 });
+
+$.fn.spin = function(opts) {
+  this.each(function() {
+    var $this = $(this),
+        data = $this.data();
+
+    if (data.spinner) {
+      data.spinner.stop();
+      delete data.spinner;
+    }
+    if (opts !== false) {
+      data.spinner = new Spinner($.extend({color: $this.css('color')}, opts)).spin(this);
+    }
+  });
+  return this;
+};
