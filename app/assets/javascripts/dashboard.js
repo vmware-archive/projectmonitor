13372510502aefab61916b0f5a157ab00c150c6d
Nicholas Greenfield & Trace Wax <pair+ngreenfield+twax@pivotallabs.com>
2012-06-20 17:43:05 -0400

    63-tile full screen layout

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -15,6 +15,9 @@ $(function() {
   else if ($('body').hasClass('tiles_48')) {
     $('.building-indicator').spin({radius:4, length:6, width:1, lines:12, top:1, left:10});
   }
+  else if ($('body').hasClass('tiles_63')) {
+    $('.building-indicator').spin({radius:4, length:4, width:1, lines:12, top:3, left:12});
+  }
   else {
     $('.building-indicator').spin({radius:6, length:7, width:2, lines:12, top:4, left:6});
   }
