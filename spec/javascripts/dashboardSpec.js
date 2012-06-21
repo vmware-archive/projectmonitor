5d96b94045b0770200333f6996258eacbe2c0e29
Nicholas Greenfield, Sabrina Staedt & Trace Wax <pair+ngreenfield+sstaedt+twax@pivotallabs.com>
2012-06-21 12:32:54 -0400

    Keep spinning even after refresh

---
+++ spec/javascripts/dashboardSpec.js
@@ -0,0 +1,25 @@
+describe('dashboard', function() {
+  beforeEach(function() {
+    var fixtures = [
+      "<ul class='projects'>",
+        "<li class='project success' id='project_1' data-id='1'>",
+          "<div class='building-indicator'></div>",
+        "</li>",
+      "</ul>"
+    ].join("\n");
+    setFixtures(fixtures);
+    $("body").addClass("dashboard").addClass("tiles_15");
+  });
+
+  it("should create a spinner", function() {
+
+    expect($('.spinner').length).toEqual(0);
+
+    var projectsCount = $("body").projectsCount();
+    var projectsCount = 48;
+    $('.building-indicator').setSpinner(projectsCount);
+
+    expect($('.spinner').length).toEqual(1);
+  });
+});
+
