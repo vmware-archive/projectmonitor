cb591d9a4c18882876b0c86c1ad215f7ce62d9e5
Mark Gangl, Sabrina Staedt & Trace Wax <pair+mgangl+sstaedt+twax@pivotallabs.com>
2012-07-05 11:02:46 -0400

    Display Pivotal Tracker iteration points for a project

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -2,5 +2,10 @@ FactoryGirl.define do
   factory :project do |f|
     name { Faker::Name.name }
     feed_url { Faker::Internet.domain_name }
+
+    factory :project_with_tracker_integration do |f|
+      tracker_project_id { "123" }
+      tracker_auth_token { "foo" }
+    end
   end
 end
