0383b79ed3752f50a5abf3cff5bb4f9c5c0b143c
Jeff Saracco & Mark Gangl <pair+jsaracco+mgangl@pivotallabs.com>
2012-07-10 12:28:08 -0400

    remove travis example url when unselecting travis as project type

--- app/assets/javascripts/projectAdmin.js
+++ app/assets/javascripts/projectAdmin.js
@@ -1,8 +1,12 @@
 $(function(){
   $('#project_type').live('change', function() {
+    var travis_example_feed_url = "http://travis-ci.org/[account]/[project]/cc.xml";
     var field_url = $("#project_feed_url");
+
     if($(this).val() == 'TravisProject' && field_url.val() === '') {
-      field_url.val('http://travis-ci.org/[account]/[project]/cc.xml');
+      field_url.val(travis_example_feed_url);
+    } else if($(this).val() != 'TravisProject' && field_url.val() === travis_example_feed_url) {
+      field_url.val("");
     }
   });
 });
