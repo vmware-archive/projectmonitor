2ca2e5a718c4defaf84a72f6313f6cc7b6a1f2bf
Kris Hicks & Mark Gangl <pair+khicks+mgangl@pivotallabs.com>
2012-07-11 10:10:15 -0400

    Clean up DashboardsController spec and code

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,17 +1,17 @@
 class DashboardsController < ApplicationController
   layout "dashboard"
 
+  respond_to :html, :json, :only => :index
+  respond_to :rss, :only => :builds
+
   def index
     @projects = DashboardGrid.generate params
-
-    respond_to do |format|
-      format.html
-      format.json { render :json => @projects }
-    end
+    respond_with @projects
   end
 
   def builds
     @projects = Project.standalone.with_statuses + AggregateProject.with_statuses
+    respond_with @projects
   end
 end
 
