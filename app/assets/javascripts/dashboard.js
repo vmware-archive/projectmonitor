e3d0b8c82231dc080c826377b878c4edc5144536
Jeff Saracco & Kris Hicks <pair+jsaracco+khicks@pivotallabs.com>
2012-07-12 15:22:45 -0400

    Use jquery-ui fade instead of spinner

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,48 +3,19 @@
 //= require twitter
 //= require versionCheck
 //= require projectCheck
+//= require projectRefresh
 //= require spin
 
 $(function() {
   VersionCheck.init();
   ProjectCheck.init();
-  var projectsCount = $("body").projectsCount();
-  $('.building-indicator').setSpinner(projectsCount);
-});
+  ProjectRefresh.init();
 
-$.fn.projectsCount = function(){
-  return parseInt($(this).data("tiles-count"), 10);
-};
-
-$.fn.setSpinner = function(projectsCount){
-  var $this = $(this);
-
-  if (projectsCount == 15) {
-    $this.spin({radius:8, length:9, width:3, lines:12, top:2, left:16});
-  }
-  else if (projectsCount == 24) {
-    $this.spin({radius:6, length:7, width:2, lines:12, top:4, left:6});
-  }
-  else if (projectsCount == 63) {
-    $this.spin({radius:4, length:4, width:1, lines:12, top:3, left:12});
-  }
-  else {
-    $this.spin({radius:4, length:6, width:1, lines:12, top:1, left:10});
-  }
-};
-
-$.fn.spin = function(opts) {
-  this.each(function() {
-    var $this = $(this),
-        data = $this.data();
-
-    if (data.spinner) {
-      data.spinner.stop();
-      delete data.spinner;
-    }
-    if (opts !== false) {
-      data.spinner = new Spinner($.extend({color: $this.css('color')}, opts)).spin(this);
-    }
+  $(document).bind("ajaxStart", function() {
+    $('#indicator').removeClass('idle');
   });
-  return this;
-};
+
+  $(document).bind("ajaxStop", function() {
+    $('#indicator').addClass('idle');
+  });
+});
