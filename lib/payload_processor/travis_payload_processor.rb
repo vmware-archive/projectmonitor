2f3ff861a01e600b15c979285f3f719235451639
Andrew Fader & Jeff Saracco <pair+afader+jsaracco@pivotallabs.com>
2012-07-16 16:26:27 -0400

    Switch Travis over to the JSON API

--- lib/payload_processor/travis_payload_processor.rb
+++ lib/payload_processor/travis_payload_processor.rb
@@ -3,19 +3,23 @@ class TravisPayloadProcessor < ProjectPayloadProcessor
 
   def parse_building_status
     building_status = BuildingStatus.new(false)
-    project_html = Nokogiri::XML.parse(payload).css('Project').first
-    building_status.building = project_html.attribute("activity").value == "Building" if project_html
+    begin
+      json = JSON.parse(payload).first
+      building_status.building = json["state"] == "started" if json
+    rescue JSON::ParserError; end
     building_status
   end
 
   def parse_project_status
     status = ProjectStatus.new(:online => false, :success => false)
-    if project_html = Nokogiri::XML.parse(payload).css('Project').first
-      status.success = project_html.attribute("lastBuildStatus").value == "Success"
-      status.url = project_html.attribute("webUrl").value
-      published_at = project_html.attribute("lastBuildTime").value
-      status.published_at = Time.parse(published_at).localtime if published_at.present?
-    end
+    begin
+      if json = JSON.parse(payload).first
+        status.success = json["result"] == 0
+        status.url = project.feed_url
+        published_at = json["finished_at"]
+        status.published_at = Time.parse(published_at).localtime if published_at.present?
+      end
+    rescue JSON::ParserError; end
     status
   end
 end
