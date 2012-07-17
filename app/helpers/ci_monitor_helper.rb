89e345f7402174145a576ceb04cc2b9fc3b42345
Trace Wax <pair+twax@pivotallabs.com>
2012-07-17 16:21:29 -0400

    Remove unused CiMonitorHelper#relative_status_messages_for

--- app/helpers/ci_monitor_helper.rb
+++ app/helpers/ci_monitor_helper.rb
@@ -1,23 +1,4 @@
 module CiMonitorHelper
-  def build_count_text_for(project)
-    return "" unless project.red?
-    count = project.red_build_count
-    "(#{count} #{count == 1 ? "build" : "builds"})"
-  end
-
-  def relative_status_messages_for(project)
-    messages = []
-    if project.online?
-      messages << ['project_published_at', project.status.published_at.present? ? "Last built #{time_ago_in_words(project.status.published_at || DateTime.now)} ago" : "Last build date unknown"]
-      if project.red?
-        messages << ['project_red_since', project.red_since.present? ? "Red since #{time_ago_in_words(project.red_since)} ago #{build_count_text_for(project)}" : "Red for some time"]
-      end
-    else
-      messages << ['project_invalid', "Could not retrieve status"]
-    end
-    messages
-  end
-
   def static_status_messages_for(project)
     messages = []
     if project.online?
@@ -29,5 +10,13 @@ module CiMonitorHelper
       messages << "Could not retrieve status"
     end
     messages
+  end
+
+  private
+
+  def build_count_text_for(project)
+    return "" unless project.red?
+    count = project.red_build_count
+    "(#{count} #{count == 1 ? "build" : "builds"})"
   end
 end
