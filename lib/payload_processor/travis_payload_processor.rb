d042c9fa08828adee23cc79a49a586f992e463cc
Andrew Fader & Mark Gangl <pair+afader+mgangl@pivotallabs.com>
2012-07-17 13:33:47 -0400

    Set status url to build_url from build ID for travis builds

--- lib/payload_processor/travis_payload_processor.rb
+++ lib/payload_processor/travis_payload_processor.rb
@@ -15,7 +15,7 @@ class TravisPayloadProcessor < ProjectPayloadProcessor
     begin
       if json = JSON.parse(payload).first
         status.success = json["result"].to_i == 0
-        status.url = project.feed_url
+        status.url = project.feed_url.gsub(".json", "/#{json["id"]}")
         published_at = json["finished_at"]
         status.published_at = Time.parse(published_at).localtime if published_at.present?
       end
