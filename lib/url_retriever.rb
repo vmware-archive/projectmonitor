7a0977eeb8b7c966c274079cca6ac1a58e755f34
Mark Gangl <pair+mgangl@pivotallabs.com>
2012-07-18 10:31:26 -0400

    make UrlRetriever a class, since it is used like a class

--- lib/url_retriever.rb
+++ lib/url_retriever.rb
@@ -1,10 +1,8 @@
 require 'net/http'
 require 'net/https'
 
-module UrlRetriever
-  extend self
-
-  def retrieve_content_at(url, username = nil, password = nil)
+class UrlRetriever
+  def self.retrieve_content_at(url, username = nil, password = nil)
     if username.present? && password.present?
       response = do_get(url) { |get| get.basic_auth(username, password) }
       if response['www-authenticate'].present?
@@ -22,7 +20,7 @@ module UrlRetriever
 
   private
 
-  def http(uri)
+  def self.http(uri)
     Net::HTTP.new(uri.host, uri.port).tap do |http|
       http.use_ssl = (uri.scheme == 'https')
       http.read_timeout = 30
@@ -30,7 +28,7 @@ module UrlRetriever
     end
   end
 
-  def do_get(url)
+  def self.do_get(url)
     uri = URI.parse(url)
     get = Net::HTTP::Get.new("#{uri.path}?#{uri.query}")
 
@@ -42,7 +40,7 @@ module UrlRetriever
     raise Net::HTTPError.new("Error: Could not connect to the remote server", nil)
   end
 
-  def digest_auth(get, response, username, password)
+  def self.digest_auth(get, response, username, password)
     challenge = HTTPAuth::Digest::Challenge.from_header(response['www-authenticate'])
     credentials = HTTPAuth::Digest::Credentials.from_challenge(challenge, {:username => username, :password => password, :uri => uri.path, :method => 'GET'})
     get['Authorization'] = credentials.to_header
