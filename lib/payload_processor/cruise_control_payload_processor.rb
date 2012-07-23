35b8479429029d8d3bb4d83679a23a534d76ef74
Andrew Fader & Mark Gangl <pair+afader+mgangl@pivotallabs.com>
2012-07-23 11:46:38 -0400

    Cleanup CruiseControlPayloadProcessor

--- lib/payload_processor/cruise_control_payload_processor.rb
+++ lib/payload_processor/cruise_control_payload_processor.rb
@@ -2,33 +2,26 @@ class CruiseControlPayloadProcessor < ProjectPayloadProcessor
   private
 
   def parse_building_status
-    building = false
-    if payload && building_payload = payload.last
-      document = Nokogiri::XML(building_payload.downcase)
-      project_element = document.at_xpath("/projects/project[@name='#{project.project_name.downcase}']")
-      building = project_element && project_element['activity'] == "building"
-    end
-    building
+    return false unless building_payload = payload.last
+
+    document = Nokogiri::XML(building_payload.downcase)
+    project_element = document.at_xpath("/projects/project[@name='#{project.project_name.downcase}']")
+    project_element && project_element['activity'] == "building"
   end
 
   def parse_project_status
-    status = ProjectStatus.new(:success => false)
-    if payload && project_payload = payload.first
-      document = Nokogiri::XML(project_payload.downcase)
-      status.success = !!(document.css('title').to_s =~ /success/)
-      if (pub_date = document.css('pubdate')).present?
-        pub_date = Time.parse(pub_date.text)
-        status.published_at = (pub_date == Time.at(0) ? Time.now : pub_date).localtime
-      end
-      if url = document.css('item/link')
-        status.url = url.text
-      end
+    return unless project_payload = payload.first
+
+    status = ProjectStatus.new
+    document = Nokogiri::XML(project_payload.downcase)
+    status.success = !!(document.css('title').to_s =~ /success/)
+    if (pub_date = document.css('pubdate')).present?
+      pub_date = Time.parse(pub_date.text)
+      status.published_at = (pub_date == Time.at(0) ? Time.now : pub_date).localtime
+    end
+    if url = document.css('item/link')
+      status.url = url.text
     end
     status
   end
 end
-
-
-
-
-
