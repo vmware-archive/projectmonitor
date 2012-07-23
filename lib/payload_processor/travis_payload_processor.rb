5c7386e375462b8b94379b0a11ed58b4a10610cb
Andrew Fader & Mark Gangl <pair+afader+mgangl@pivotallabs.com>
2012-07-23 11:16:45 -0400

    cleanup on TravisPayloadProcessor

--- lib/payload_processor/travis_payload_processor.rb
+++ lib/payload_processor/travis_payload_processor.rb
@@ -2,26 +2,24 @@ class TravisPayloadProcessor < ProjectPayloadProcessor
   private
 
   def parse_building_status
-    building = false
-    if parse_payload!
-      building = payload["state"] == "started" if payload
-    end
-    building
+    return false unless convert_payload!
+
+    payload["state"] == "started"
   end
 
   def parse_project_status
-    status = ProjectStatus.new(:success => false)
-    if parse_payload!
-      status.success = payload["result"].to_i == 0
-      status.url = project.feed_url.gsub(".json", "/#{payload["id"]}")
-      published_at = payload["finished_at"]
-      status.build_id = payload["id"]
-      status.published_at = Time.parse(published_at).localtime if published_at.present?
-    end
+    return unless convert_payload!
+
+    status = ProjectStatus.new
+    status.success = payload["result"].to_i == 0
+    status.url = project.feed_url.gsub(".json", "/#{payload["id"]}")
+    published_at = payload["finished_at"]
+    status.build_id = payload["id"]
+    status.published_at = Time.parse(published_at).localtime if published_at.present?
     status
   end
 
-  def parse_payload!
+  def convert_payload!
     @parsed ||=
       begin
         self.payload = payload.fetch("payload", payload) if payload.respond_to?(:fetch)
