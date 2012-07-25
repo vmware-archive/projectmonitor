7706f41fdfce2589132f4578250c93816af7ab41
Mark Gangl <pair+mgangl@pivotallabs.com>
2012-07-25 11:26:50 -0400

    change references of cimonitor to projectmonitor

--- chef/cookbooks/cimonitor/recipes/ci.rb
+++ chef/cookbooks/cimonitor/recipes/ci.rb
@@ -9,11 +9,11 @@ username = ENV['SUDO_USER'].strip
 raise "CI_CONFIG does not contain a jenkins_dir" unless CI_CONFIG["jenkins_dir"]
 
 execute "make project dir" do
-  command "mkdir -p #{CI_CONFIG["jenkins_dir"]}/jobs/cimonitor"
+  command "mkdir -p #{CI_CONFIG["jenkins_dir"]}/jobs/projectmonitor"
   user username
 end
 
-template "#{CI_CONFIG["jenkins_dir"]}/jobs/cimonitor/config.xml" do
-  source "jenkins-cimonitor-config.xml.erb"
+template "#{CI_CONFIG["jenkins_dir"]}/jobs/projectmonitor/config.xml" do
+  source "jenkins-projectmonitor-config.xml.erb"
   owner username
 end
