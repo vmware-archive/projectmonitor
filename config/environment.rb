655ca89b0eadbc54f57a9096b183519ceef32cb8
Mark Gangl <pair+mgangl@pivotallabs.com>
2012-07-25 11:26:50 -0400

    change references of cimonitor to projectmonitor

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 ::RED_NOTIFICATION_EMAILS = ["notify@example.com"]
-::SYSTEM_ADMIN_EMAIL = "Pivotal CiMonitor <pivotal-cimonitor@example.com>"
+::SYSTEM_ADMIN_EMAIL = "Pivotal Project Monitor <pivotal-projectmonitor@example.com>"
 
 
 CiMonitor::Application.initialize!
