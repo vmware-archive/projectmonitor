6b773de75970d01b4aaef5aa7b69e30d8001687c
Andrew Fader & John Barker <pair+afader+jbarker@pivotallabs.com>
2012-08-02 17:31:11 -0400

    Ensure no leakage in the project json feed

---
+++ spec/decorators/project_feed_decorator_spec.rb
@@ -0,0 +1,10 @@
+describe ProjectFeedDecorator do
+
+  describe "#as_json" do
+    subject { ProjectFeedDecorator.new(Project.new(name: "foo")).as_json['project'].keys }
+
+    it { should include :tag_list }
+    it { should_not include %w[auth_username auth_password tracker_auth_token deprecated_feed_url deprecated_latest_status_id] }
+  end
+
+end
