cbfda06654de6f294d12264951f4c020c7fa9917
Andrew Fader & John Barker <pair+afader+jbarker@pivotallabs.com>
2012-08-06 17:08:46 -0400

    Give project type labels a more meaningful name

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,9 +1,9 @@
 module ProjectsHelper
-  PROJECT_TYPE_NAMES = [CruiseControlProject, JenkinsProject, TeamCityRestProject, TeamCityProject, TeamCityChainedProject, TravisProject]
+  PROJECT_TYPE_NAMES = [CruiseControlProject, JenkinsProject, TeamCityRestProject, TeamCityChainedProject, TeamCityProject, TravisProject]
 
   def project_types
     [['', '']] + PROJECT_TYPE_NAMES.map do |type_class|
-      [type_class.name.titleize, type_class.name]
+      [t("project_types.#{type_class.name.underscore}"), type_class.name]
     end
   end
 
