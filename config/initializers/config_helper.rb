64bcd2e8b7708daf8e90476a4ee8984d8d5aab5d
Andrew Fader <pair+afader@pivotallabs.com>
2012-08-08 15:18:37 -0400

    Handle Heroku env variable 'false' string as boolean

--- config/initializers/config_helper.rb
+++ config/initializers/config_helper.rb
@@ -3,7 +3,8 @@ def config_for(key, required = false)
 
   key = key.to_s
   if ENV.key?(key)
-    handle_value.(ENV[key])
+    ENV[key] == "false" ? v = false : v = ENV[key]
+    handle_value.(v)
   elsif Rails.configuration.respond_to?(key)
     handle_value.(Rails.configuration.send(key))
   elsif required
