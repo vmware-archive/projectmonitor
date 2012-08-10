79134298b5689f3e33cfc8c7cfc728caf902d1d0
Ian Zabel & Ryan Ong <pair+izabel+rong@pivotallabs.com@pivotallabs.com>
2012-08-10 15:14:06 -0400

    Fix the migrations for legacy DBs

--- db/migrate/20120808144638_add_guids_to_existing_projects.rb
+++ db/migrate/20120808144638_add_guids_to_existing_projects.rb
@@ -1,15 +1,24 @@
-class AddGuidsToExistingProjects < ActiveRecord::Migration
-  Project = Class.new ActiveRecord::Base
-  TeamCityChainedProject = Class.new ActiveRecord::Base
-  TeamCityProject = Class.new ActiveRecord::Base
-  TeamCityRestProject = Class.new ActiveRecord::Base
-  JenkinsProject = Class.new ActiveRecord::Base
-  TravisProject = Class.new ActiveRecord::Base
-  CruiseControlProject = Class.new ActiveRecord::Base
+require "securerandom"
 
+class Project < ActiveRecord::Base
+end
+class TeamCityChainedProject < Project
+end
+class TeamCityProject < Project
+end
+class TeamCityRestProject < Project
+end
+class JenkinsProject < Project
+end
+class TravisProject < Project
+end
+class CruiseControlProject < Project
+end
+
+class AddGuidsToExistingProjects < ActiveRecord::Migration
   def up
     Project.all.each do |project|
-      project.send(:generate_guid)
+      project.guid = SecureRandom.uuid
       next unless project.save
     end
   end
