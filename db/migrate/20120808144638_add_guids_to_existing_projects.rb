106f32a7787e9c04a988b27529b4672ccefef03d
Andrew Fader & John Barker <pair+afader+jbarker@pivotallabs.com>
2012-08-13 09:37:53 -0400

    Improve solution for STI ActiveRecord overriding future implementation of class

--- db/migrate/20120808144638_add_guids_to_existing_projects.rb
+++ db/migrate/20120808144638_add_guids_to_existing_projects.rb
@@ -1,21 +1,11 @@
 require "securerandom"
 
-class Project < ActiveRecord::Base
-end
-class TeamCityChainedProject < Project
-end
-class TeamCityProject < Project
-end
-class TeamCityRestProject < Project
-end
-class JenkinsProject < Project
-end
-class TravisProject < Project
-end
-class CruiseControlProject < Project
-end
-
 class AddGuidsToExistingProjects < ActiveRecord::Migration
+  Project = Class.new ActiveRecord::Base do
+    acts_as_taggable
+    self.inheritance_column = nil
+  end
+
   def up
     Project.all.each do |project|
       project.guid = SecureRandom.uuid
