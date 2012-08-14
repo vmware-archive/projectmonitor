4c2a560dc93f534c3815a9ad46b6d8abe1893894
John Barker & Michael Chinigo <pair+jbarker+mchinigo@pivotallabs.com>
2012-08-14 16:32:30 -0400

    Update existing records in the importing
    - Export the guid and webhooks_enabled attributes for projects
    - If a project exists with a specific guid, update instead of making a
      new project
    - Wrap importing in a transaction

--- spec/lib/config_export_spec.rb
+++ spec/lib/config_export_spec.rb
@@ -4,7 +4,7 @@ describe ConfigExport do
 
   context 'given a full set of configuration records' do
     let(:aggregate_project) { stub_model(AggregateProject, name: 'agg', id: 1, tag_list: [])}
-    let(:solo_project) { FactoryGirl.build(:jenkins_project, name: 'Foo', tag_list: %w[foo bar baz]) }
+    let(:solo_project) { FactoryGirl.create(:jenkins_project, name: 'Foo', tag_list: %w[foo bar baz]) }
     let(:aggregated_project) { FactoryGirl.build(:travis_project, name: 'Led', aggregate_project_id: 1) }
     let(:projects) do
       [solo_project, aggregated_project]
@@ -18,39 +18,25 @@ describe ConfigExport do
     end
 
     it 'can export and import those records' do
-      AggregateProject
-        .should_receive(:create!)
-        .with('name' => 'agg', 'enabled' => true, 'tag_list' => [])
-        .and_return(aggregate_project)
-      Project
-        .should_receive(:create!)
-        .with('name' => 'Foo',
-              'deprecated_feed_url' => nil,
-              'auth_username' => nil,
-              'auth_password' => nil,
-              'enabled' => true,
-              'type' => 'JenkinsProject',
-              'polling_interval' => nil,
-              'aggregate_project_id' => nil,
-              'deprecated_latest_status_id' => nil,
-              'code' => nil,
-              'tracker_project_id' => nil,
-              'tracker_auth_token' => nil,
-              'cruise_control_rss_feed_url' => nil,
-              'jenkins_base_url' => 'http://www.example.com',
-              'jenkins_build_name' => 'project',
-              'team_city_base_url' => nil,
-              'team_city_build_id' => nil,
-              'team_city_rest_base_url' => nil,
-              'team_city_rest_build_type_id' => nil,
-              'travis_github_account' => nil,
-              'travis_repository' => nil,
-              'tag_list' => ['foo', 'bar', 'baz'])
-      Project
-        .should_receive(:create!)
-        .with(hash_including('name' => 'Led', 'aggregate_project_id' => 1))
+      expect do
+        export = ConfigExport.export
 
-      ConfigExport.import ConfigExport.export
+        solo_project.name = Faker::Company.name
+        solo_project.tag_list = nil
+        solo_project.save!
+
+        ConfigExport.import export
+      end.to change(Project, :count).by(1)
+
+      aggregate_project = AggregateProject.last
+      aggregate_project.name.should == 'agg'
+
+      solo_project.reload.name.should == 'Foo'
+      solo_project.tag_list.should == %w[foo bar baz]
+
+      aggregated_project = Project.last
+      aggregated_project.name.should == 'Led'
+      aggregated_project.aggregate_project_id.should == aggregate_project.id
     end
   end
 
