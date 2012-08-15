2c1781cdc7c1250d78574c6b4b964ad06f4245d7
John Barker & Michael Chinigo <pair+jbarker+mchinigo@pivotallabs.com>
2012-08-15 11:24:10 -0400

    Change config helper to make it more test friendly

--- config/initializers/config_helper.rb
+++ config/initializers/config_helper.rb
@@ -1,13 +1,13 @@
-def config_for(key, required = false)
-  handle_value = ->(value) { block_given? ? yield(value) : value }
+class ConfigHelper
+  def self.get(key)
+    handle_value = ->(value) { block_given? ? yield(value) : value }
 
-  key = key.to_s
-  if ENV.key?(key)
-    ENV[key] == "false" ? v = false : v = ENV[key]
-    handle_value.(v)
-  elsif Rails.configuration.respond_to?(key)
-    handle_value.(Rails.configuration.send(key))
-  elsif required
-    raise ArgumentError.new "Missing required configuration parameter '#{key}'"
+    key = key.to_s
+    if ENV.key?(key)
+      ENV[key] == "false" ? v = false : v = ENV[key]
+      handle_value.(v)
+    elsif Rails.configuration.respond_to?(key)
+      handle_value.(Rails.configuration.send(key))
+    end
   end
 end
