dd999ad902904663d17a03693d19314a222eb232
James Robert Somers and Ryan Ong <pair+jrsomers+rong@pivotallabs.com>
2012-11-05 12:07:07 -0500

    remove crontab example and use whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,12 @@
 every :day, :at => '3:00am' do
-  rake "truncate:payload_log_entries"
+  rake "truncate:payload_log_entries", :output => "payload_log_entries.log"
+  rake "truncate:project_statuses", :output => "project_statuses.log"
+end
+
+every 3.minutes do
+  rake "cimonitor:fetch_statuses", :output => "fetch_statuses.log"
+end
+
+every :weekday, :at => '8:30am' do
+  rake "cimonitor:red_over_one_day_notification", :output => "red_notify.log"
 end
