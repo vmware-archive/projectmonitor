2a488c9cbb53c28c184d617494d8bd680c41e692
Ryan Ong <pair+rong@pivotallabs.com>
2012-11-05 16:27:01 -0500

    update log files for cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,12 @@
 every :day, :at => '3:00am' do
-  rake "truncate:payload_log_entries", :output => "payload_log_entries.log"
-  rake "truncate:project_statuses", :output => "project_statuses.log"
+  rake "truncate:payload_log_entries", :output => "log/payload_log_entries.log"
+  rake "truncate:project_statuses", :output => "log/project_statuses.log"
 end
 
 every 3.minutes do
-  rake "cimonitor:fetch_statuses", :output => "fetch_statuses.log"
+  rake "cimonitor:fetch_statuses", :output => "log/fetch_statuses.log"
 end
 
 every :weekday, :at => '8:30am' do
-  rake "cimonitor:red_over_one_day_notification", :output => "red_notify.log"
+  rake "cimonitor:red_over_one_day_notification", :output => "log/red_notify.log"
 end
