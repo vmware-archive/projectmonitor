e4612b602e83aac48f7e8ee333026d117d864f58
James Robert Somers and Jeff Saracco <pair+jrsomers+jeff@pivotallabs.com>
2012-11-06 11:59:23 -0500

    Remove non-existent rake task from cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,7 +6,3 @@ end
 every 3.minutes do
   rake "cimonitor:fetch_statuses", :output => "log/fetch_statuses.log"
 end
-
-every :weekday, :at => '8:30am' do
-  rake "cimonitor:red_over_one_day_notification", :output => "log/red_notify.log"
-end
