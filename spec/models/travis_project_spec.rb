b4b313e03f542ed8d9c83088839d8ef44db4ca47
James Robert Somers and Jeff Saracco <pair+jrsomers+jeff@pivotallabs.com>
2012-11-08 11:06:07 -0500

    Update Travis API

--- spec/models/travis_project_spec.rb
+++ spec/models/travis_project_spec.rb
@@ -25,21 +25,22 @@ describe TravisProject do
     end
   end
 
-  its(:feed_url) { should == 'https://travis-ci.org/account/project/builds.json' }
+  its(:feed_url) { should == 'https://api.travis-ci.org/repos/account/project/builds.json' }
   its(:project_name) { should == 'account' }
-  its(:build_status_url) { should == 'https://travis-ci.org/account/project/builds.json' }
+  its(:build_status_url) { should == 'https://api.travis-ci.org/repos/account/project/builds.json' }
 
   describe '#current_build_url' do
     subject { project.current_build_url }
+
     context "webhooks are enabled" do
       let(:project) { FactoryGirl.build(:travis_project, webhooks_enabled: true, parsed_url: 'foo.gov') }
       it { should == 'foo.gov'}
     end
+
     context "webhooks are disabled" do
       let(:project) { FactoryGirl.build(:travis_project) }
 
-    it { should == 'https://travis-ci.org/account/project' }
+      it { should == 'https://api.travis-ci.org/repos/account/project' }
     end
   end
-
 end
