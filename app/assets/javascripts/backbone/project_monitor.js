bbaf4d4ae0beedb22b63939d715e0aa4a009b6cf
Andy Pliszka and Brent Wheeldon <pair+andy+brent@pivotallabs.com>
2012-11-19 14:37:46 -0500

    Adds Backbone namespaces to window.ProjectMonitor

--- app/assets/javascripts/backbone/project_monitor.js
+++ app/assets/javascripts/backbone/project_monitor.js
@@ -1,9 +1,14 @@
+//= require_self
 //= require_tree ./templates
 //= require_tree ./models
 //= require_tree ./views
 //= require_tree ./routers
 
 window.ProjectMonitor = {
+  Models: {},
+  Collections: {},
+  Routers: {},
+  Views: {},
   Window: {
     reload: function () {
       return window.location.reload();
