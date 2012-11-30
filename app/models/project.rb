3b19ea2efa8259d66bb4d201aa7dededb3934ded
Andy Pliszka and Jeff Saracco <pair+andy+jeff@pivotallabs.com>
2012-11-30 10:03:47 -0500

    Moves time_since_last_build

--- app/models/project.rb
+++ app/models/project.rb
@@ -173,6 +173,23 @@ class Project < ActiveRecord::Base
     self.guid = SecureRandom.uuid
   end
 
+  def time_since_last_build
+    return unless published_at = latest_status.try(:published_at)
+
+    since_last_build = Time.now - published_at
+    if published_at <= 1.week.ago
+      (since_last_build / 1.week).floor.to_s + "w"
+    elsif published_at <= 1.day.ago
+      (since_last_build / 1.day).floor.to_s + "d"
+    elsif published_at <= 1.hour.ago
+      (since_last_build / 1.hour).floor.to_s + "h"
+    elsif published_at <= 1.minute.ago
+      (since_last_build / 1.minute).floor.to_s + "m"
+    else
+      since_last_build.floor.to_s + "s"
+    end
+  end
+
   private
 
   def self.project_attribute_prefix
