c0596c6d6be4ac251f3d50485a5dd942a598e4cd
Andy Pliszka and Jeff Saracco <pair+andy+jeff@pivotallabs.com>
2012-11-30 10:03:47 -0500

    Moves time_since_last_build

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -25,22 +25,4 @@ class ProjectDecorator < ApplicationDecorator
 
     klass
   end
-
-  def time_since_last_build
-    return unless published_at = latest_status.try(:published_at)
-
-    since_last_build = Time.now - published_at
-    if published_at <= 1.week.ago
-      (since_last_build / 1.week).floor.to_s + "w"
-    elsif published_at <= 1.day.ago
-      (since_last_build / 1.day).floor.to_s + "d"
-    elsif published_at <= 1.hour.ago
-      (since_last_build / 1.hour).floor.to_s + "h"
-    elsif published_at <= 1.minute.ago
-      (since_last_build / 1.minute).floor.to_s + "m"
-    else
-      since_last_build.floor.to_s + "s"
-    end
-  end
-
 end
