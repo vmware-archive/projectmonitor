d86d9421221b2fe52b409804336d821c51e5acc5
Andy Pliszka and Jeff Saracco <pair+andy+jeff@pivotallabs.com>
2012-12-03 11:01:20 -0500

    Adds Home request spec

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
-feature "home" do
+feature "home", js: true do
+  let!(:project) { FactoryGirl.create(:project) }
+
+  it "should render tile collection" do
+    visit "/home"
+    page.should have_selector(".tiles")
+    page.should have_selector(".tile")
+  end
 end
