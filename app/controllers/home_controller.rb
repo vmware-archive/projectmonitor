f654a2d36153ba1c495498dc52094b3fa4db6185
Andy Pliszka and Jeff Saracco <pair+andy+jeff@pivotallabs.com>
2012-12-03 10:51:18 -0500

    Update HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
+  layout 'dashboard'
 
   respond_to :json, :only => [:index]
 
   def index
+    aggregate_projects = AggregateProject.displayable(params[:tags])
+    standalone_projects =  Project.standalone.displayable(params[:tags])
+    projects = standalone_projects.concat(aggregate_projects).sort_by { |p| p.code.downcase }
+
+    @tiles = projects
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,6 +2,27 @@ require 'spec_helper'
 require 'time'
 
 describe HomeController do
+  let!(:projects) { [FactoryGirl.create(:jenkins_project)] }
+  let!(:aggregate_projects) { [FactoryGirl.create(:aggregate_project)] }
+
   describe "#index" do
+    let(:tags) { 'bleecker' }
+
+    before do
+      AggregateProject.stub(:displayable).and_return(aggregate_projects)
+      Project.stub_chain(:standalone, :displayable).and_return(projects)
+      projects.stub_chain(:concat, :sort_by).and_return(projects + aggregate_projects)
+    end
+
+    it "should render collection of projects as JSON" do
+      get :index, format: :json
+      response.body.should == (projects + aggregate_projects).to_json
+    end
+
+    it 'gets a collection of aggregate projects by tag' do
+      AggregateProject.should_receive(:displayable).with(tags)
+      projects.stub(:take).and_return(projects)
+      get :index, tags: tags
+    end
   end
 end
