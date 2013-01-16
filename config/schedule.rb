3f5d610b57166a15122199f9460c436cd4ac013f
David Lee and David Tengdin <pair+dlee+dtengdin@pivotallabs.com>
2013-01-16 16:00:47 -0500

    Revert "story ID 42570711 refactor external dependency polling to cache results"
    This reverts commit 512f821cb6d4ac43c6728ae65a05d9e079e93fed.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,8 @@
 every :day, :at => '3:00am' do
   rake "truncate:payload_log_entries", :output => "log/payload_log_entries.log"
   rake "truncate:project_statuses", :output => "log/project_statuses.log"
-  rake "dependency:truncate_old_statuses", :output => "log/dependency_statuses.log"
 end
 
 every 3.minutes do
   rake "cimonitor:fetch_statuses", :output => "log/fetch_statuses.log"
-end
-
-every 30.seconds do
-  rake "dependency:fetch_statuses"
 end
--- db/migrate/20130115195626_add_raw_status_to_external_dependency.rb
+++
@@ -1,5 +0,0 @@
-class AddRawStatusToExternalDependency < ActiveRecord::Migration
-  def change
-    add_column :external_dependencies, :raw_response, :string
-  end
-end
