391bdb29eb09017533383a99a99801ae0d7608a5
Dennis Cahillane and Sabrina Staedt <pair+dcahillane+sstaedt@pivotallabs.com>
2013-02-22 11:39:08 -0500

    attempt 2

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -1,7 +1,7 @@
 require 'vcr'
 
 VCR.configure do |c|
-  c.ignore_hosts = '127.0.0.1', 'localhost'
+  c.ignore_hosts '127.0.0.1', 'localhost'
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
