ce6d5067d44810fe0c46b5024684e79249dd8d1a
Dimitri Roche <pair+dimroc@pivotallabs.com>
2013-02-22 17:57:05 -0500

    Revert vcr gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@ RSpec.configure do |config|
 
   config.include Devise::TestHelpers, :type => :controller
 
+  config.extend VCR::RSpec::Macros
+
   config.treat_symbols_as_metadata_keys_with_true_values = true
 
   config.order = 'random'
