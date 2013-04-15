a7cba3ed97e552d9eeedc14fba5dcb5bcc5feac9
Andy Pliszka and Joe Masilotti <pair+apliszka+jmasilotti@pivotallabs.com>
2013-04-15 17:07:53 -0400

    Refactors IPWhitelistedController

--- lib/ip_whitelisted_controller.rb
+++ lib/ip_whitelisted_controller.rb
@@ -17,14 +17,24 @@ module IPWhitelistedController
   end
 
   def restrict_ip_address
+    restrict_access! unless client_ip_in_whitelist(client_ip)
+  end
+
+  def client_ip_in_whitelist(ip)
+    ip_whitelist.any? { |address| address.include? ip }
+  end
+
+  def client_ip
     client_ip_address_str = if ConfigHelper.get(:ip_whitelist_request_proxied)
         (request.env['HTTP_X_FORWARDED_FOR'] || '').split(',').last.try(:strip)
       else
         request.env['REMOTE_ADDR']
       end
 
-    client_ip_address = client_ip_address_str ? IPAddr.new(client_ip_address_str) : nil
-    allowed_ips = ConfigHelper.get(:ip_whitelist).map { |ip_str| IPAddr.new(ip_str) }
-    restrict_access! unless allowed_ips.any? { |ip| ip.include? client_ip_address }
+    client_ip_address_str ? IPAddr.new(client_ip_address_str) : nil
+  end
+
+  def ip_whitelist
+    ConfigHelper.get(:ip_whitelist).map { |ip_str| IPAddr.new(ip_str) }
   end
 end
