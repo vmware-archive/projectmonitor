fb6eb1afec221d3da8fb180f64955c24e1acbd11
Andy Pliszka and Joe Masilotti <pair+apliszka+jmasilotti@pivotallabs.com>
2013-04-16 17:12:55 -0400

    Cleans up project edit view spec

--- spec/views/projects/edit.html.haml_spec.rb
+++ spec/views/projects/edit.html.haml_spec.rb
@@ -1,19 +1,21 @@
 require 'spec_helper'
 
-describe 'projects/new' do
-  before { @project = FactoryGirl.build(:travis_project) }
-
+describe 'projects/edit' do
   context 'Travis Project' do
-    it 'has a visible fieldset for travis project fields' do
+    let(:project) { FactoryGirl.create(:travis_project) }
+
+    before do
+      assign(:project, project)
       render
+    end
+
+    it 'has a visible fieldset for travis project fields' do
       rendered.should have_css('fieldset#TravisProject')
       rendered.should_not have_css('fieldset#TravisProject.hide')
-
       rendered.should_not have_css('fieldset#build_setup #branch_name.hide')
     end
 
     it 'should render the alternative project specific fields as hidden' do
-      render
       rendered.should have_css('fieldset#CruiseControlProject.hide')
       rendered.should have_css('fieldset#JenkinsProject.hide')
       rendered.should have_css('fieldset#TeamCityRestProject.hide')
