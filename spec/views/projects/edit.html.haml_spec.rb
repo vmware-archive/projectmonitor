82db56218d905f8a3a4c3e6814ea73a4b1fbb76a
Andy Pliszka and Joe Masilotti <pair+apliszka+jmasilotti@pivotallabs.com>
2013-04-17 10:44:38 -0400

    Refactors project edit page view spec

--- spec/views/projects/edit.html.haml_spec.rb
+++ spec/views/projects/edit.html.haml_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe 'projects/edit' do
+
   describe "information about project creator" do
     context "when the creator is missing" do
       it "does not include creator's information" do
         project = FactoryGirl.create(:travis_project, creator: nil)
         assign(:project, project)
         render
-        rendered.should_not include "Creator"
+        page.should have_no_content "Creator"
       end
     end
 
@@ -21,11 +22,11 @@ describe 'projects/edit' do
       end
 
       it "has creator's name" do
-        rendered.should have_content project.creator.name
+        page.should have_content project.creator.name
       end
 
       it "has creator's email" do
-        rendered.should have_content project.creator.email
+        page.should have_content project.creator.email
       end
     end
   end
@@ -39,17 +40,17 @@ describe 'projects/edit' do
     end
 
     it 'has a visible fieldset for travis project fields' do
-      rendered.should have_css('fieldset#TravisProject')
-      rendered.should_not have_css('fieldset#TravisProject.hide')
-      rendered.should_not have_css('fieldset#build_setup #branch_name.hide')
+      page.should have_css('fieldset#TravisProject')
+      page.should_not have_css('fieldset#TravisProject.hide')
+      page.should_not have_css('fieldset#build_setup #branch_name.hide')
     end
 
     it 'should render the alternative project specific fields as hidden' do
-      rendered.should have_css('fieldset#CruiseControlProject.hide')
-      rendered.should have_css('fieldset#JenkinsProject.hide')
-      rendered.should have_css('fieldset#TeamCityRestProject.hide')
-      rendered.should have_css('fieldset#TeamCityProject.hide')
-      rendered.should have_css('fieldset#SemaphoreProject.hide')
+      page.should have_css('fieldset#CruiseControlProject.hide')
+      page.should have_css('fieldset#JenkinsProject.hide')
+      page.should have_css('fieldset#TeamCityRestProject.hide')
+      page.should have_css('fieldset#TeamCityProject.hide')
+      page.should have_css('fieldset#SemaphoreProject.hide')
     end
   end
 end
