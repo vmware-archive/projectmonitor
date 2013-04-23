ad0d390eeeb1b80fd93d3a1a151cc4a790dd9c17
Graham Siener <siener@gmail.com>
2013-04-23 13:04:22 -0400

    VCR should ignore imgur uploads

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -2,6 +2,7 @@ require 'vcr'
 
 VCR.configure do |c|
   c.ignore_localhost = true
+  c.ignore_hosts 'api.imgur.com'
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.hook_into :fakeweb
   c.configure_rspec_metadata!
