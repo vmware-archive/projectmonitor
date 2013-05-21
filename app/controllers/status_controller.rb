afd72fd258d71c0917f339d3ed97c3cb5f5dd273
Graham Siener <gsiener@pivotallabs.com>
2013-05-21 14:08:22 -0400

    Ignore payload posts to a project that isn't found, return a 404 [Finishes #50290555]

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,17 +2,19 @@ class StatusController < ApplicationController
   skip_filter :restrict_ip_address, :authenticate_user!
 
   def create
-    project = Project.find_by_guid(params.delete(:project_id))
+    if project = Project.find_by_guid(params.delete(:project_id))
+      payload = project.webhook_payload
+      payload.remote_addr = request.env["REMOTE_ADDR"]
+      payload.webhook_status_content = request.body.read
 
-    payload = project.webhook_payload
-    payload.remote_addr = request.env["REMOTE_ADDR"]
-    payload.webhook_status_content = request.body.read
+      log = PayloadProcessor.new(project, payload).process
+      log.update_method = "Webhooks"
+      log.save!
 
-    log = PayloadProcessor.new(project, payload).process
-    log.update_method = "Webhooks"
-    log.save!
-
-    project.save!
-    head :ok
+      project.save!
+      head :ok
+    else
+      head :not_found
+    end
   end
 end
