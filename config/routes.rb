7b4e16dd38ae1e7b3bf418c335e050ea01b69c8b
Andy Pliszka and Trevor John <pair+apliszka+tjohn@pivotallabs.com>
2013-07-08 13:53:16 -0400

    Updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 ProjectMonitor::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks", :sessions => "sessions" }
-  match 'builds.rss' => "home#builds", format: :rss
-  match 'projects/validate_tracker_project'
-  match 'projects/validate_build_info'
-  match 'version' => 'versions#show'
-  match 'github_status' => 'home#github_status', format: :json
-  match 'heroku_status' => 'home#heroku_status', format: :json
-  match 'rubygems_status' => 'home#rubygems_status', format: :json
+  get 'builds' => "home#builds", format: :rss
+  get 'projects/validate_tracker_project'
+  get 'projects/validate_build_info'
+  get 'version' => 'versions#show'
+  get 'github_status' => 'home#github_status', format: :json
+  get 'heroku_status' => 'home#heroku_status', format: :json
+  get 'rubygems_status' => 'home#rubygems_status', format: :json
 
   resource :configuration, only: [:show, :create, :edit], controller: "configuration"
   resources :users, :only => [:new, :create]
@@ -28,9 +28,9 @@ ProjectMonitor::Application.routes.draw do
   end
 
   authenticate :user do
-    match "/jobs" => DelayedJobWeb, :anchor => false
+    get "/jobs" => DelayedJobWeb, :anchor => false
   end
 
-  match 'styleguide' => 'home#styleguide'
+  get 'styleguide' => 'home#styleguide'
   root :to => 'home#index'
 end
