f65359f8ab49027570d280986b56e0be15422a06
Andy Pliszka <apliszka@pivotallabs.com>
2013-07-11 16:53:58 -0400

    Cleans up config files

--- config/application.rb
+++ config/application.rb
@@ -2,56 +2,27 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module ProjectMonitor
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
     config.from_file 'settings.yml'
-
-    # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib #{config.root}/lib/payload)
+    config.encoding = 'utf-8'
+    config.i18n.fallbacks = true
 
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+    config.serve_static_assets = true
 
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # JavaScript files you want as :defaults (application.js is always included).
-    #config.action_view.javascript_expansions[:defaults] = %w(jquery.min jquery_ujs)
-
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
-
-    # Enable the asset pipeline
+    config.assets.compile = true
     config.assets.enabled = true
+    config.assets.version = '1.1'
     config.assets.initialize_on_precompile = false
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
     config.assets.paths << Rails.root.join('app','assets','skins')
 
-    # Allow setting of mail options from the configuration
     config.action_mailer.default_url_options = Rails.configuration.emailer_host.to_hash.symbolize_keys
+    config.secret_key_base = 'project_monitor'
 
-    # Secret key
-    config.secret_key_base = "project_monitor"
-
-    # Set rails cache to use memory
     config.cache_store = :memory_store
+    config.static_cache_control = "public, max-age=3600"
   end
 end
