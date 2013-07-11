ad7ac76cb33c6bf884ad0416d23b1f664de44992
Andy Pliszka <apliszka@pivotallabs.com>
2013-07-11 16:53:58 -0400

    Cleans up config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,36 +1,19 @@
 ProjectMonitor::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
 
   config.eager_load = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
-  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations
-  config.active_record.migration_error = :page_load
-
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
   config.assets.debug = true
 
-  # enable source maps for sass http://fonicmonkey.net/2013/03/25/native-sass-scss-source-map-support-in-chrome-and-rails/
   config.sass.debug_info = true
-  config.sass.line_comments = false # source maps don't get output if this is true
+  config.sass.line_comments = false
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  config.max_status = 15
+  config.active_record.migration_error = :page_load
+
+  config.action_mailer.raise_delivery_errors = false
 end
