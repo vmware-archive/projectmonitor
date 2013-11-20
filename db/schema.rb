69e58ab79150fea96e5da1ed65af9bd37b7a0b44
Graham Siener <gsiener@pivotallabs.com>
2013-11-20 10:08:06 -0500

    Schema now uses fancy new hash syntax

--- db/schema.rb
+++ db/schema.rb
@@ -9,46 +9,49 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130416212824) do
+ActiveRecord::Schema.define(version: 20131015123350) do
 
-  create_table "aggregate_projects", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "aggregate_projects", force: true do |t|
     t.string   "name"
-    t.boolean  "enabled",                  :default => true
-    t.datetime "created_at",                                 :null => false
-    t.datetime "updated_at",                                 :null => false
+    t.boolean  "enabled",               default: true
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "code"
-    t.string   "location",   :limit => 20
+    t.string   "location",   limit: 20
   end
 
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0
+    t.integer  "attempts",   default: 0
     t.text     "handler"
     t.text     "last_error"
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
     t.string   "locked_by"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "queue"
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
-  create_table "external_dependencies", :force => true do |t|
+  create_table "external_dependencies", force: true do |t|
     t.string   "name"
     t.string   "status"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "raw_response"
   end
 
-  create_table "payload_log_entries", :force => true do |t|
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+  create_table "payload_log_entries", force: true do |t|
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "project_id"
     t.string   "status"
     t.string   "update_method"
@@ -57,11 +60,11 @@ ActiveRecord::Schema.define(:version => 20130416212824) do
     t.string   "error_type"
   end
 
-  add_index "payload_log_entries", ["created_at"], :name => "index_payload_log_entries_on_created_at"
-  add_index "payload_log_entries", ["project_id", "created_at"], :name => "index_payload_log_entries_on_project_id_and_created_at"
+  add_index "payload_log_entries", ["created_at"], name: "index_payload_log_entries_on_created_at", using: :btree
+  add_index "payload_log_entries", ["project_id", "created_at"], name: "index_payload_log_entries_on_project_id_and_created_at", using: :btree
 
-  create_table "project_statuses", :force => true do |t|
-    t.boolean  "success",      :default => false, :null => false
+  create_table "project_statuses", force: true do |t|
+    t.boolean  "success",      default: false, null: false
     t.string   "url"
     t.datetime "published_at"
     t.integer  "project_id"
@@ -71,21 +74,21 @@ ActiveRecord::Schema.define(:version => 20130416212824) do
     t.integer  "build_id"
   end
 
-  create_table "projects", :force => true do |t|
+  create_table "projects", force: true do |t|
     t.string   "name"
     t.string   "deprecated_feed_url"
     t.string   "auth_username"
     t.string   "auth_password"
-    t.boolean  "enabled",                                    :default => true
-    t.boolean  "building",                                   :default => false, :null => false
+    t.boolean  "enabled",                                 default: true
+    t.boolean  "building",                                default: false, null: false
     t.string   "type"
     t.integer  "aggregate_project_id"
     t.integer  "deprecated_latest_status_id"
     t.string   "code"
-    t.string   "deprecated_location",          :limit => 20
+    t.string   "deprecated_location",          limit: 20
     t.string   "tracker_project_id"
     t.string   "tracker_auth_token"
-    t.integer  "current_velocity",                           :default => 0,     :null => false
+    t.integer  "current_velocity",                        default: 0,     null: false
     t.string   "last_ten_velocities"
     t.boolean  "tracker_online"
     t.string   "cruise_control_rss_feed_url"
@@ -97,7 +100,7 @@ ActiveRecord::Schema.define(:version => 20130416212824) do
     t.string   "team_city_rest_build_type_id"
     t.string   "travis_github_account"
     t.string   "travis_repository"
-    t.boolean  "online",                                     :default => false
+    t.boolean  "online",                                  default: false
     t.string   "guid"
     t.boolean  "webhooks_enabled"
     t.string   "tracker_validation_status"
@@ -107,22 +110,22 @@ ActiveRecord::Schema.define(:version => 20130416212824) do
     t.string   "tddium_auth_token"
     t.string   "tddium_project_name"
     t.string   "notification_email"
-    t.boolean  "verify_ssl",                                 :default => true
+    t.boolean  "verify_ssl",                              default: true
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "stories_to_accept_count"
     t.integer  "open_stories_count"
     t.string   "build_branch"
-    t.text     "iteration_story_state_counts",               :default => "{}"
+    t.text     "iteration_story_state_counts",            default: "{}"
     t.integer  "creator_id"
     t.string   "circleci_auth_token"
     t.string   "circleci_project_name"
     t.string   "circleci_username"
   end
 
-  add_index "projects", ["aggregate_project_id"], :name => "index_projects_on_aggregate_project_id"
+  add_index "projects", ["aggregate_project_id"], name: "index_projects_on_aggregate_project_id", using: :btree
 
-  create_table "taggings", :force => true do |t|
+  create_table "taggings", force: true do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
     t.string   "taggable_type"
@@ -132,25 +135,25 @@ ActiveRecord::Schema.define(:version => 20130416212824) do
     t.string   "context"
   end
 
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
-  create_table "tags", :force => true do |t|
+  create_table "tags", force: true do |t|
     t.string "name"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "login",                  :limit => 40
-    t.string   "name",                   :limit => 100, :default => ""
-    t.string   "email",                  :limit => 100
-    t.string   "encrypted_password",                    :default => "", :null => false
-    t.string   "password_salt",          :limit => 40
+  create_table "users", force: true do |t|
+    t.string   "login",                  limit: 40
+    t.string   "name",                   limit: 100, default: ""
+    t.string   "email",                  limit: 100
+    t.string   "encrypted_password",                 default: "", null: false
+    t.string   "password_salt",          limit: 40
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",                      default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -159,7 +162,7 @@ ActiveRecord::Schema.define(:version => 20130416212824) do
     t.string   "uid"
   end
 
-  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["login"], name: "index_users_on_login", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
