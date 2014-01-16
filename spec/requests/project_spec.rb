3e30fa99758834f5061f763c1ef31f5405ff0bfb
John Ryan and Ryan Moran <pair+jryan+rmoran@pivotallabs.com>
2014-01-16 09:15:28 -0800

    Ensures project/build_validation_info uses the semantically correct HTTP verb PATCH instead of POST
    [finishes #59499742]

--- spec/requests/project_spec.rb
+++ spec/requests/project_spec.rb
@@ -6,7 +6,7 @@ describe "Project" do
       project = FactoryGirl.create(:project)
       ProjectUpdater.stub(:update).and_return(PayloadLogEntry.new(error_text: "Build unsuccessful"))
 
-      post "/projects/validate_build_info", project: project.attributes.merge(auth_password: "password")
+      patch "/projects/validate_build_info", project: project.attributes.merge(auth_password: "password")
       expect(response.body).to match /Build unsuccessful/
     end
   end
