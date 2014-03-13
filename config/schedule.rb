b4dd99cba30941b7092542722533ee9018294cce
James Wilford <james@shutl.co.uk>
2014-03-13 15:18:02 +0000

    remove non-existent rake tasks from schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,14 +1,9 @@
 every :day, at: '3:00am' do
   rake "truncate:payload_log_entries", output: "log/payload_log_entries.log"
   rake "truncate:project_statuses", output: "log/project_statuses.log"
-  rake "dependency:truncate_old_statuses", output: "log/dependency_statuses.log"
   rake "projectmonitor:remove_unused_tags"
 end
 
 every 3.minutes do
   rake "projectmonitor:fetch_statuses", output: "log/fetch_statuses.log"
-end
-
-every 1.minute do
-  rake "dependency:fetch_statuses"
 end
