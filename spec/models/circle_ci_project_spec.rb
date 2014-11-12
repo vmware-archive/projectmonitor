a39d83e7a99840784e2b0f3c9fb518f1828ea9f5
Brent Wheeldon <brent.wheeldon@gmail.com>
2014-11-12 15:29:21 -0500

    Support branches for CircleCI.

--- spec/models/circle_ci_project_spec.rb
+++ spec/models/circle_ci_project_spec.rb
@@ -8,20 +8,39 @@ describe CircleCiProject, :type => :model do
     it { is_expected.to validate_presence_of(:circleci_username) }
   end
 
-  subject { build(:circleci_project) }
-
   describe 'model' do
     it { is_expected.to validate_presence_of(:ci_auth_token) }
     it { is_expected.to validate_presence_of(:ci_build_identifier) }
   end
 
   describe 'accessors' do
-    describe '#feed_url' do
-      it { expect(subject.feed_url).to eq('https://circleci.com/api/v1/project/username/a-project?circle-token=b5bb9d8014a0f9b1d61e21e796d78dccdf1352f2') }
-    end
+    subject { build(:circleci_project, branch_name: branch_name) }
+    let(:branch_name) { "foo" }
 
-    describe '#build_status_url' do
-      it { expect(subject.build_status_url).to eq('https://circleci.com/api/v1/project/username/a-project?circle-token=b5bb9d8014a0f9b1d61e21e796d78dccdf1352f2') }
+    describe "branch related" do
+      context "when a branch is specified" do
+        let(:branch_name) { "foo" }
+
+        describe '#feed_url' do
+          it { expect(subject.feed_url).to eq('https://circleci.com/api/v1/project/username/a-project/tree/foo?circle-token=b5bb9d8014a0f9b1d61e21e796d78dccdf1352f2') }
+        end
+
+        describe '#build_status_url' do
+          it { expect(subject.build_status_url).to eq('https://circleci.com/api/v1/project/username/a-project/tree/foo?circle-token=b5bb9d8014a0f9b1d61e21e796d78dccdf1352f2') }
+        end
+      end
+
+      context "when a branch is not specified" do
+        let(:branch_name) { nil }
+
+        describe '#feed_url' do
+          it { expect(subject.feed_url).to eq('https://circleci.com/api/v1/project/username/a-project/tree/master?circle-token=b5bb9d8014a0f9b1d61e21e796d78dccdf1352f2') }
+        end
+
+        describe '#build_status_url' do
+          it { expect(subject.build_status_url).to eq('https://circleci.com/api/v1/project/username/a-project/tree/master?circle-token=b5bb9d8014a0f9b1d61e21e796d78dccdf1352f2') }
+        end
+      end
     end
 
     describe '#ci_build_identifier' do
