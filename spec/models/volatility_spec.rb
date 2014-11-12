27f0ba0a83e6b2b22e6bb720b8dad01b3588ef4a
Oli Peate <opeate@pivotal.io>
2014-11-12 16:26:37 +0000

    [tidyup] remove old RSpec 2 matchers / non-standard expect syntax

--- spec/models/volatility_spec.rb
+++ spec/models/volatility_spec.rb
@@ -6,22 +6,22 @@ describe Volatility, :type => :model do
 
     context "with simple velocity" do
       let(:last_ten_velocities) { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }
-      expect_it { to eq 55 }
+      it { is_expected.to eq 55 }
     end
 
     context "with historical velocity" do
       let(:last_ten_velocities) { [0, 0, 4, 0, 0, 7, 4, 15, 0, 0] }
-      expect_it { to eq 163 }
+      it { is_expected.to eq 163 }
     end
 
     context "with zero velocity" do
       let(:last_ten_velocities) { [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
-      expect_it { to eq 0 }
+      it { is_expected.to eq 0 }
     end
 
     context "with no velocity object" do
       let(:last_ten_velocities) { [] }
-      expect_it { to eq 0 }
+      it { is_expected.to eq 0 }
     end
   end
 end
