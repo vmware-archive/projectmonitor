2f507d08c9675b47cb9600dd71951d654b81a588
Lenny Koepsell and Michael McCormick <mmccormick@pivotal.io>
2015-02-24 16:40:03 -0800

    Backfills tests around Codeship regex matching
    [#87311834]

--- spec/lib/payload/codeship_payload_spec.rb
+++ spec/lib/payload/codeship_payload_spec.rb
@@ -10,15 +10,25 @@ describe CodeshipPayload do
 
   it_behaves_like "a JSON payload"
 
-  it 'filters builds by branch name' do
-    content = payload.convert_content!(fixture_content)
-    expect(content.length).to eq(1)
-  end
+  context 'filtering on the branch name' do
+    it 'can match the exact branch name' do
+      content = CodeshipPayload.new(45716, 'master').convert_content!(fixture_content)
+      expect(content.length).to eq(1)
+      expect(content.first['branch']).to eq('master')
+    end
 
-  it 'filters no branches if branch name is empty' do
-    payload = CodeshipPayload.new(45716, '')
-    content = payload.convert_content!(fixture_content)
-    expect(content.length).to eq(2)
+    it 'can accept a ruby-flavor regex' do
+      content = CodeshipPayload.new(45716, '.*other.*').convert_content!(fixture_content)
+      expect(content.length).to eq(1)
+      expect(content.first['branch']).to eq('some-other-branch')
+    end
+
+    it 'does not happen when the branch name is empty' do
+      content = CodeshipPayload.new(45716, '').convert_content!(fixture_content)
+      expect(content.length).to eq(2)
+      expect(content.first['branch']).to eq('some-other-branch')
+      expect(content.last['branch']).to eq('master')
+    end
   end
 
   describe '#parse_url' do
