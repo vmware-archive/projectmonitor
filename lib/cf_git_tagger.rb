d3cffb40fb85e4fb9874fbcecb4acbd9d06faf3e
Anthony Dreessen <adreessen@pivotal.io>
2015-03-27 17:26:53 -0700

    Fixes some errors in the rake cf_deploy task

--- lib/cf_git_tagger.rb
+++ lib/cf_git_tagger.rb
@@ -2,7 +2,7 @@ class CF_git_tagger
   def tag_commit_with_message(tag, commit_sha, message)
     puts "will tag commit with command 'git tag #{tag} #{commit_sha} -m #{message}'"
 
-    `git tag #{tagName} #{sha} -m "pushing to #{actual_env}"`
-    `git push origin #{tagName}`
+    `git tag #{tag} #{commit_sha} -m "#{message}"`
+    `git push origin #{tag}`
   end
 end
