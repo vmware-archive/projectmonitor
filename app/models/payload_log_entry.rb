eedd7b0058b512e841d8713bed1924e2bd943a18
Anthony Dreessen <anthonydreessen@gmail.com>
2015-04-10 11:45:16 -0700

    Now deletes most recent failed log entry with same error message
    attempting to reduce the amount of stored log entries to cut down on databse size
    
    [finishes #92241638]

--- app/models/payload_log_entry.rb
+++ app/models/payload_log_entry.rb
@@ -1,5 +1,6 @@
 class PayloadLogEntry < ActiveRecord::Base
   belongs_to :project
+  after_create :remove_duplicates
 
   default_scope { order('created_at DESC') }
   scope :reverse_chronological, -> () { order('created_at DESC') }
@@ -10,5 +11,15 @@ class PayloadLogEntry < ActiveRecord::Base
 
   def to_s
     "#{update_method} #{status}"
+  end
+
+  def remove_duplicates
+    if status == 'failed'
+      PayloadLogEntry.where(project_id: project_id).order(created_at: :asc).limit(2).each do |entry|
+        if entry.id != id && entry.status == 'failed' && entry.error_text == error_text
+          entry.delete()
+        end
+      end
+    end
   end
 end
