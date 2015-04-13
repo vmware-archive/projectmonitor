6c4980e86ade38b6c1eee160fccb5d81c0a0d5d5
Anthony Dreessen <anthonydreessen@gmail.com>
2015-04-13 10:04:04 -0700

    updated clockwork.rb to use the defaults set in truncate.rake

--- clockwork.rb
+++ clockwork.rb
@@ -8,6 +8,6 @@ module Clockwork
     `rake #{job}`
   end
 
-  every(1.day, 'truncate:payload_log_entries[2]')
+  every(1.day, 'truncate:payload_log_entries')
   every(1.day, 'truncate:project_statuses[1000]')
 end
