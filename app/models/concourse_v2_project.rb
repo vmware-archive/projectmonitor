3284ec32ded18aecf2b7c5e687969dc554aaa736
Ian Fisher <loktar@gmail.com>
2016-10-05 10:22:50 -0700

    Added Concourse v2 project type
    [#131657693]
    
    Signed-off-by: Elana Koren <ekoren@pivotal.io>

---
+++ app/models/concourse_v2_project.rb
@@ -0,0 +1,18 @@
+class ConcourseV2Project < Project
+
+  validates_presence_of :ci_build_identifier, :ci_base_url, :concourse_pipeline_name
+
+  alias_attribute :build_status_url, :feed_url
+
+  def feed_url
+    "#{ci_base_url}/api/v1/teams/main/pipelines/#{concourse_pipeline_name}/jobs/#{ci_build_identifier}/builds"
+  end
+
+  def fetch_payload
+    ConcoursePayload.new(feed_url)
+  end
+
+  def self.project_specific_attributes
+    ['concourse_pipeline_name', 'ci_build_identifier', 'ci_base_url']
+  end
+end
